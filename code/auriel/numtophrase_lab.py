99e8e019ee6f4d97a5f9fb5b2138661df101647d
LK-NightWalker <auriel.cofield@yahoo.com>
2021-11-17 17:34:07 -0500

    modified number to phrase

--- code/auriel/numtophrase_lab.py
+++ code/auriel/numtophrase_lab.py
@@ -27,7 +27,6 @@ else:
 
 
 # Version 2 of Lab
-import random
 
 # Create a list for ones, teens and tens and hundreds
 ones = { 0: '', 1 : 'one', 2 : 'two', 3 : 'three', 4 :'four', 5 : 'five', 6 :'six', 7 : 'seven', 8 : 'eight', 9 :'nine', 10 : 'ten'}
@@ -42,6 +41,7 @@ num = random.randint(0, 999)
 one_digit = num % 10
 ten_digit = (num % 100) - one_digit
 hun_digit = (num - ten_digit) - one_digit
+teens_digit = num - hun_digit
 
 print(num)
 
@@ -57,5 +57,7 @@ elif num > 19 and num < 100:
     print(tens[ten_digit] + '-' + ones[one_digit])
 elif num == 100 or num == 200 or num == 300 or num == 400 or num == 500 or num == 600 or num == 700 or num == 800 or num == 900:
     print(hundreds[num])
+elif teens_digit > 10 and teens_digit < 20:
+    print(hundreds[hun_digit] + ' and ' + teens[teens_digit])
 else:
     print(hundreds[hun_digit] + ' and ' + tens[ten_digit] + ' ' + ones[one_digit])
