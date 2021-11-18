ea38964d2e4903bb048037f0c7418988a2a019a0
Dustin <94391156+IronWill12@users.noreply.github.com>
2021-11-18 10:52:34 -0700

    labs 3 4 and 5

--- code/Dustin/Lab_05.py
+++ code/Dustin/Lab_05.py
@@ -5,9 +5,23 @@ card_two = input("What is your second card? ")
 card_three = input("What is your third card? ")
 
 
+def conversion(card):
+      if card == 'A':
+            card = 1
+            print(card,"card was an ace")
+      elif card == 'J' or card == 'Q' or card == 'K':
+            card = 10
+            print(card,"card was Jack")
+      else:
+            card = int(card)
+            print(card, "card was a 5")
+      return card
 
 
-total_value = (card_one + card_two + card_three)
+
+
+
+total_value = (conversion(card_one) + conversion(card_two) + conversion(card_three))
 if total_value < 17:
       print("Hit!")
 elif total_value < 20:
