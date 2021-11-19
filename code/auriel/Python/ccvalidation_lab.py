278ccbc39a3cd561abdd9aad31575b9e112edf3a
LK-NightWalker <auriel.cofield@yahoo.com>
2021-11-19 16:43:42 -0500

    made changes to some labs and uploading CC Validation

--- code/auriel/Python/ccvalidation_lab.py
+++ code/auriel/Python/ccvalidation_lab.py
@@ -9,20 +9,21 @@ def cc_validation():
         print('Please enter a 16 digit card number.')
 
     sliced = cc_number[0:15] # Removes last digit og list
-    print(sliced)
+
     sliced.reverse() #Reverse the order of the list
-    print(sliced)
+
     sliced[::2] = [n * 2 for n in sliced[::2]] # Double every other number in list. starting with index 0
-    print(sliced)
+    
     subtract = [ n - 9 if n > 9 else n for n in sliced] # Subtract 9 for any interger in list that is over 9
-    print(subtract)
+    
     sumof_subtract = sum(subtract) # Gets the sum off all the integers in the list of subtract
-    print(sumof_subtract)
+    
     last_digit = sumof_subtract % 10
-    print(last_digit)
-    if last_digit == cc_number[15]:
-        print(f'This is a valid Credit Card!')
+    
+    if last_digit == cc_number[15]: # If the last digit of the sum is equal to 16th digit of the credit card number entered
+        print(f'This IS a valid Credit Card!')
     else:
         print(f'This is NOT a valid Credit Credit! Please try again.')
     
+
 cc_validation()
