8f7ccf6c7c7280a55992a028c56aae1c98d93fe8
Vanessa <v.d.chacon@gmail.com>
2021-11-22 10:17:59 -0800

    update

--- code/vanessa/lab06.py
+++ code/vanessa/lab06.py
@@ -8,7 +8,7 @@ winnings = {1  : 4,
             6   : 25000000
 }
 
-def winning_numbers():
+def pick6():
     '''generate six random winning numbers'''
     results= []
     i = 0
@@ -17,35 +17,21 @@ def winning_numbers():
         i = i + 1
     return results
 print("")
-print(f"Winning numbers: {winning_numbers()}")
+print(f"Winning numbers: {pick6()}")
 print("")
-
-print (winning_numbers())
-
-
-
-
-
 
 Tickets_bought = 0
 while -10 < Tickets_bought :
-    def pick6():
-        '''generate six random numbers'''
-        results6= []
-        i = 0
-        while i < 6:
-            results6.append(random.randint(1,99))
-            i = i + 1
-        return results6
+   
     
     Tickets_bought = int(Tickets_bought) - 2
     print(f"Ticket numbers: {pick6()}   tickets bought ${Tickets_bought}")
 
-def num_matches(winning_numbers, pick6):
-    """return the number of matches between the 2 """
-    print(winning_numbers[1] == pick6[1])
+# def num_matches(winning_numbers, pick6):
+#     """return the number of matches between the 2 """
+#     print(winning_numbers[1] == pick6[1])
 
-print(num_matches(winning_numbers, pick6))
+# print(num_matches(winning_numbers, pick6))
 
 
     
--- code/vanessa/lab06testing.py
+++ code/vanessa/lab06testing.py
@@ -1,8 +1,6 @@
 import random
 
-
-
-def winning_numbers():
+def pick6():
     '''generate six random winning numbers'''
     results= []
     i = 0
@@ -10,10 +8,52 @@ def winning_numbers():
         results.append(random.randint(1,99))
         i = i + 1
     return results
+winning_ticket = pick6()
+#print(f"Winning ticket: {winning_ticket}")
+
+my_tickets = []
+Tickets_bought = 0
+
+while -8 < Tickets_bought:
+    my_tickets.append(pick6())
+    Tickets_bought = Tickets_bought -2   
+#print(f"Ticket numbers: {my_tickets}   tickets bought ${Tickets_bought}")
+
+
+
+
+def num_matches(winning_ticket, my_tickets):
+    """("return") the number of matches between the 2"""
+    #if winning_ticket[0]== my_tickets [0]:
+    print (winning_ticket[0])
+    print (my_tickets[0])
+    # if winning_ticket[1]== my_tickets [1]:
+    #         print ("match")
+    # if winning_ticket[2]== my_tickets [2]:
+    #         print ("match")
+    # if winning_ticket[3]== my_tickets [3]:
+    #         print ("match")
+    # if winning_ticket[4]== my_tickets[4]:
+    #         print ("match")
+    # if winning_ticket[5]== my_tickets[5]:
+    #         print ("match")       
+    # else:
+    #         print("no match")    
+    # print(num_matches(winning_ticket,my_tickets))
+
+num_matches(winning_ticket, my_tickets)
+
+
+# def num_matches(winner, ticket_list):
+#     """
+#     return the number of matches between the 2 tickets
+#     """
+#     ...
+
+# # range(len(winning_ticket)) == range(len(pick6)):
+# #         print ("match")
+# #print(num_matches(winning_numbers,pick6))"""
+
+
+
     
-for i in range(len(winning_numbers)):
-        print(winning_numbers[i])
-print("")
-defined_winnings= (f"Winning numbers: {winning_numbers()}")
-print(defined_winnings)
-print(defined_winnings [0])
