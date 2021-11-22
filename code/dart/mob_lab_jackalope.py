3eafaef255f37ae5dce9e60d004b79d1483d14e7
d'Artagnan Childers <dartagnanchilders@dArtagnans-MacBook-Pro.local>
2021-11-22 11:17:33 -0800

    jackalope_lab_dc_ac_ca

---
+++ code/dart/mob_lab_jackalope.py
@@ -0,0 +1,31 @@
+
+
+years_counter = 0
+
+population = [0, 0]
+
+
+
+# while len(population) < 10:
+#     years_counter += 1
+#     for i, age in enumerate(population):
+#         population[i] += 1
+#     if age >= 4 and age <= 8:
+        
+        
+
+
+# print(population, years_counter)
+
+
+""""""""""""""""""""""""""""""""
+
+while len(population) < 10:
+    years_counter += 1
+    for i, age in enumerate(population):
+        population[i] += 1
+    if len(population) >= 10:
+        break
+    else:
+        print(population, years_counter)
+        years_counter += years_counter
