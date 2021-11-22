5bd763e620088c199622aced94c30445f9e6f935
travisjackson3 <travisjackson3@gmail.com>
2021-11-22 12:07:48 -0800

    completed lab

--- code/mobs/a-l-t-mob/a-l-t-mob.py
+++ code/mobs/a-l-t-mob/a-l-t-mob.py
@@ -1,28 +1,30 @@
 # Jackalop mob lab
 
-jackalopes = 2 #Have to figure out how to add jackalopes
-age = 0
-population = []
+jackalopes = 0 #Have to figure out how to add jackalopes
+year = 0
+population = [0, 0]
 
-while jackalopes <= 1000:
-    for jackalope in population:
-        age +=1
-        if age >= 4 and age <= 8:
-            jackalopes =  2 + jackalopes
-            # print(age)
+
+while len(population) < 1000:
+
+    for i, jackalope in enumerate(population):
+                
+        
+        population[i] = jackalope + 1
+        
+        #checks breeding age
+        if jackalope >= 4 and jackalope <= 8:
+
             population.append(jackalopes) #add 2 0's
-            print(jackalopes)
-        if age > 9:
-            population.pop(jackalope)
-            print(population)
-# for loop to add new population between correct ages
-            # .append(population)
-# if # > 9, .remove
 
-# # 
-# while population <= 1000:
-#     age +=1
-#     if age >= 4 and age <= 8:
-#         print(age)
-#         if age == 10:
-#             break
+
+        #jackalope dies
+        if jackalope > 9:
+
+            population.pop(i)
+            
+
+    year += 1
+
+    print(population)
+    print(year)
