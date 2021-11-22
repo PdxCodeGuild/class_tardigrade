9bfbdd3cdd52bc928e332b6bdaaf49d2d51693c1
Dustin <94391156+IronWill12@users.noreply.github.com>
2021-11-22 11:08:22 -0700

    update on lab

--- code/Dustin/test.py
+++ code/Dustin/test.py
@@ -1,6 +1,20 @@
-colors = ["purple", "green", "red"]
+"""Credit card validation"""
 
-for i in range(len(colors)):
-    print(colors[i])
+tested_num = input("enter credit card number here: ")
+list_num = list(tested_num)
+for i in range(0, len(list_num)):
+    list_num[i] = int(list_num[i])
+print(list_num)
+check_num = list_num[15]
+
+list_num = list_num[0:15:]
+list_num.reverse()
+list_num = 
+print(list_num)
+
+
+
+
+
 
 
