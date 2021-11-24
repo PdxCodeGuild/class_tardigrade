968596ac88510e73b842fa846a18422d7d6b9fde
LK-NightWalker <auriel.cofield@yahoo.com>
2021-11-24 15:37:35 -0500

    add ROT13 Cipher lab

--- code/auriel/Python/test.py
+++ code/auriel/Python/test.py
@@ -1,28 +1,19 @@
-def cc_validation():
-    user = input('Please input Credit Card Number: ')
-    cc_number = list(user)
-    cc_number = [int(i) for i in cc_number]
 
-    if len(cc_number) == 16:
-        print(f'You have enetered: {user}')
-    else:
-        print('Please enter a 16 digit card number.')
+lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    sliced = cc_number[0:15] # Removes last digit og list
-    print(sliced)
-    sliced.reverse() #Reverse the order of the list
-    print(sliced)
-    sliced[::2] = [n * 2 for n in sliced[::2]] # Double every other number in list. starting with index 0
-    print(sliced)
-    subtract = [ n - 9 if n > 9 else n for n in sliced] # Subtract 9 for any interger in list that is over 9
-    print(subtract)
-    sumof_subtract = sum(subtract) # Gets the sum off all the integers in the list of subtract
-    print(sumof_subtract)
-    last_digit = sumof_subtract % 10 # Gets the second digit of the sum
-    print(last_digit)
-    if last_digit == cc_number[15]:
-        print(f'This is a valid Credit Card!')
-    else:
-        print(f'This is NOT a valid Credit Credit! Please try again.')
+def cipher():
+    user = input('Please input a phrase: ')
+    phrase = list(user)
+    coded = []
+
+    for i, letter in enumerate(phrase):
+        for l, alpha in enumerate(lowercase_letters):
+            if letter == alpha and letter < lowercase_letters[13]:
+                coded.append(lowercase_letters[l + 13])
+            if letter == alpha and letter > lowercase_letters[12]:
+                coded.append(lowercase_letters[l - 13])
+
+    final_output = ''.join(coded)
+    print(final_output)
     
-cc_validation()
+cipher()
