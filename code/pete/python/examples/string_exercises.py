4d56192388cfd5e7edb75020c3b0d740917f2bdd
pjz987 <pwj2012@gmail.com>
2021-11-29 09:30:44 -0800

    string exercises

---
+++ code/pete/python/examples/string_exercises.py
@@ -0,0 +1,55 @@
+"""String Literals"""
+
+
+"""Escape Sequences"""
+
+
+"""ord() and chr()"""
+
+
+"""Concatenation & Multiplication"""
+
+
+"""Access: string[i]"""
+school = 'PDXCodeGuild'
+# using bracket notation, print out the 'X' in 'PDXCodeGuild'
+
+
+"""Slicing: string[i:j:k]"""
+# using bracket notation, print out the 'Code' from 'PDXCodeGuild'
+
+
+"""String Methods"""
+# loop over this list, and use string.startswith() and string.endswith()
+# to make sure that every sentence is exactly like this: 'Coding is fun.'
+# use string.capitalize() and concatenation to replace any strings
+sentences = [
+    'Coding is fun.',
+    'Coding is fun',
+    'Coding is fun.',
+    'coding is fun.',
+    'coding is fun',
+    'Coding is fun',
+]
+
+
+"""Split and Join"""
+students = 'shad, auriel, travis, dustin, christian, alnardo, vanessa, lujock'
+
+# use string.split() to split the student names into a list
+
+# sort the names alphabetically
+
+# use string.join(list) to get a new string of the sorted names
+
+
+"""F-Strings"""
+
+
+"""string in string"""
+
+
+"""Looping Over Strings"""
+
+
+"""Docstrings"""
