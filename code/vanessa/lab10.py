9d704c97f5e4cc58a82db8cd1cb505576ea115d5
Vanessa <v.d.chacon@gmail.com>
2021-12-01 09:53:54 -0800

    lab 10

--- code/vanessa/lab10.py
+++ code/vanessa/lab10.py
@@ -8,33 +8,32 @@
 
 # Index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
 # English	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
-# ROT+13	n	o	p	q	r	s	t	u	v	w	x	y	z	a	b	c	d	e	f	g	h	i	j
+# ROT+13	n	o	p	q	r	s	t	u	v	w	x	y	z	a	b	c	d	e	f	g	h	i	j   k   l   m
 
 english=["a","b","c","d","e","f",
 "g","h","i","j","k","l","m","n",
 "o","p","q","r","s","t","u","v",
 "w","x","y","z"]
-rot= english.copy()
-
-for j in range(len(rot)):
-    rot.insert(j,(english[j]+13))
-    print (j)
-    print(rot)
-    
 
 
 
+user_input = input("Please type word to encode: ")
 
-# word_to_encode = input("Please type word to encode: ")
-# word_to_encode =list(word_to_encode)
-# print(word_to_encode)
-# def encrypt(entered_word):
-#     new_word=[]
-#     for j in range(len(entered_word)):
-#             entered_word.insert(j,english[j+13])
-#             new_word.append(entered_word[j])
-#     print(new_word)
+
+# print(user_input)
+def encrypt(entered_word):
+    new_word= ""
+    print(entered_word)
+    for i in entered_word:
+        index = english.index(i)
+        print(index)
+        if index > 13:
+            new_letter = english[index-13]
+            new_word += new_letter
+        else:
+            new_letter = english[index+13]
+            new_word += new_letter
+    print(new_word)
             
-
-# (encrypt(rot))
+(encrypt(user_input))
     
