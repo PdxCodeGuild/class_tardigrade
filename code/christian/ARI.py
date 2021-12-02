bfb3007a931be7c46eba80ce0870098db240802f
christian <chrisalv27@yahoo.com>
2021-12-01 16:27:26 -0800

    completed ROT. Started ARI

---
+++ code/christian/ARI.py
@@ -0,0 +1,18 @@
+
+
+with open('book.txt', 'r',encoding='utf-8') as f:
+    contents = f.read()
+    # print(contents)
+
+
+    char_count = 0
+    for x in contents:
+        print(x)
+#         if x == 'a':
+#             char_count +1
+
+# print(char_count)
+
+        
+
+
