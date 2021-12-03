67ac02fed3c8ab05b10de498b0de946a8b24ffc9
christian <chrisalv27@yahoo.com>
2021-12-02 16:31:18 -0800

    worked ARI lab

--- code/christian/ARI.py
+++ code/christian/ARI.py
@@ -4,14 +4,54 @@ with open('book.txt', 'r',encoding='utf-8') as f:
     contents = f.read()
     # print(contents)
 
+    
+    
+    sen_count = 0
 
-    char_count = 0
-    for x in contents:
-        print(x)
-#         if x == 'a':
-#             char_count +1
+    for sentence in contents:
+        if sentence.endswith('.'):
+            sen_count += 1
+        elif sentence.endswith('!'):
+            sen_count +=1
 
-# print(char_count)
+print(sen_count)
+        
+
+    
+    
+    
+    
+    
+#     letter_count = 0
+#     for letter in contents:
+#         if letter == letter:
+#             letter_count += 1
+# print(letter_count)
+        
+        
+    
+    
+    # words = contents.split('\n')
+    # # print(len(words))
+    # words = ','.join(words)
+    # #print(words)
+    # words = words.split(' ')
+    # print(words)
+
+#     word_count = 0
+
+#     for word in (words):
+#         word_count +=1
+
+# print(word_count)
+
+    
+  
+
+
+
+    
+
 
         
 
