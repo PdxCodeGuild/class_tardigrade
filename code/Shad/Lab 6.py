444f012edbb0c6c9564ecae9b8ae834f51760ae9
Shad-Wi <94399458+Shad-Wi@users.noreply.github.com>
2021-12-02 23:21:38 -0500

    g

--- code/Shad/Lab 6.py
+++ code/Shad/Lab 6.py
@@ -1,36 +1,99 @@
-import random
+from random import randint
 
-tickets = []
-def pick6():
-    ter =0
+# tickets = []
+# def pick6():
+#     ter = 0
     
-    while ter < 6:
-        ter = ter + 1
-        tick = random.randint(0,99)
-        tickets.append(tick)
-        if ter == 6:
-            return tickets
-        
+#     while ter < 6:
+#         ter = ter + 1
+#         tick = randint(0,99)
+#         tickets.append(tick)
+#         if ter == 6:
+#             return tickets
 
-pick6()
-print(tickets)
+
+ticket = []
+def pick6_2():
+    
+
+    for _ in range(6):
+        ticket.append(randint(1, 99))
+
+    return ticket
+            
+
+  
+
+def num_matches():
+    for _ in range(100000):
+        winning_ticket=entry = pick6_2()
+     
+        if ticket == winning_ticket:
+            return 6
+
+        elif ticket[0:4] == winning_ticket[0:4] or ticket[0][1][2][3][4] == winning_ticket[0][1][2][3][4]:
+            print('you win $1000000')
+            return 4
+        else:
+            print('you lose')
+        return entry and ticket
+
+num_matches()
+    
+
+    # elif ticket[0][1][2][3][4][5] == winning_ticket[0][1][2][3][4][5]:
+    #     print('you win $7')
+
+    # elif all_tickets[0:2] == winnning_ticket[0:2]:
+    #     print('you win $100')
+
+    # elif all_tickets[0:3] == winnning_ticket[0:3]:
+    #     print('you win $50000')
+
+
+
+ 
+
+  
+
+
+# Creating the winning ticket and start balance as 0.
+winning_ticket = pick6_2()
+balance = 0
+
+
+# Creating and comparing 100,000 tickets with the winning ticket.
+for _ in range(100000):
+    # Create the entry from the 100,000
+    entry = pick6_2()
+
+    # Withdrawing $2 for the ticket.
+    balance -= 2
+
+    # Call num_matches(winning_ticket, entry).
+
+
+#     #
+
+# pick6()
+# print(tickets)
 
 
 #print(winnning_ticket)
-all_tickets = []
+# all_tickets = []
 
-def winning():
-    x = 0
-    r=0
-    while x < 100000:
-        r = r + 1
-        x += 1
-        winnum = random.randint(0,99)
-        all_tickets.append(winnum)
-        if r == 6:      
-            return all_tickets
-winning()
-print(all_tickets)
+# def winning():
+#     x = 0
+#     r=0
+#     while x < 100000:
+#         r = r + 1
+#         x += 1
+#         winnum = random.randint(0,99)
+#         all_tickets.append(winnum)
+#         if r == 6:      
+#             return all_tickets
+# winning()
+# print(all_tickets)
 
 # if all_tickets[0] == winnning_ticket[0]:
 #     print('you win $4')
@@ -47,8 +110,6 @@ print(all_tickets)
 #else:
     # print('loser')   
 
-
- 
  
 
 # winning = []
--- code/Shad/lab 12.py
+++ code/Shad/lab 12.py
@@ -1,25 +1,119 @@
+
+# with open('code/pete/python/examples/csv/cities.csv', 'r') as f:
+#     lines = f.read().split('\n')
+# print(lines)
+
+# cities = [ # you want to build a list of dictionaries (like this)
+#            # from the string you get from the csv file
+#     {'name': 'portland', 'food': 'jojos', 'climate': 'temperate'},
+#     {'name': 'kansas city', 'food': 'bbq', 'climate': 'temperate'},
+#     {'name': 'lima', 'food': 'ceviche', 'climate': 'tropical'},
+# ]
+
+
+
+import sys
 import csv
 
 i = 'code/Shad/contact.csv'
-contact = {}
+
 with open(i, 'r',encoding='utf-8') as file:
     lines = file.read().split('\n')
     print(lines)
 
-    # with open(i, 'w',newline='') as row:
-    #     title =['name','fruit','color']
+    print('\n')
+
+
+
+    headers = lines[0].split(',')
+
+
+con = [] 
+
+for i in range(1,len(lines)):
+    #iterate over the people
+    #make a dictionary for each person
+    contact = {}
+    for h in range(len(headers)):
+        #itereate over person contact details
+        #turn details into a list
+        #match hearder with corresponding details
         
-    #     write1= csv.DictWriter(row, title = title )
-    #     write1.writeheader( )
-    #     write1.writerow({'name': 'mike','fruit': 'apple','color': 'blue'})
-    #     write1.writerow({'name': 'ike','fruit': 'pear','color': 'red'})
-    #     write1.writerow({'name': 'tanjiro','fruit': 'passionfruit','color': 'green'})
-    #     print(write1)
+        person=lines[i].split(',')
+        contact[headers[h]] = person[h]
+    con.append(contact)
+        # print(headers[h])
+
+print(con)
+    # print(lines[i])
+# for lis in lines:
+#     
+#     # contact_list=lis.split(',')
+#     # print(contact_list[1])
+#     # for i, u in enumerate(contact_list):
+#     #     print(u,i)
+#     # contact_list=lis.split(',')
+# print(con)
 
 
-    with open("contact.csv", "w", newline="\n") as read1:
+
+# k = 'code/Shad/contact.csv'
+
+def main():
+   menu()
+
+def menu():
+    print()
+
+    choice = int(input("""
+                      1: Read File
+                      2: Update the file
+                      3: Quit
+
+                      Please choose: """))
+
+    if choice == 1:
+        read_file()
+    elif choice == 2:
+        Update_the_File()
+    elif choice == 3:
+        sys.exit
+    else:
+        print("Please try again")
+        menu()
+
+def read_file():
+    with open(k, "r") as read:
+        view = csv.reader(read, delimiter=',')
+        for x in view:
+            print(x)
+        read.close()
+
+    
+def Update_the_File():
+    
+    new = []
+    
+
+    with open(k, "w", newline="\n") as read1:
         view1 = csv.writer(read1, delimiter = ",")
         view1.writerow(input(" "))
         
+        
+        
+      
+        
+   
+   
+main()
+        
 
 
+
+# be on your branch git checkout [[branch-name]] to switch between branches
+# git pull origin main pulls main and merges it into your branch
+# do some stuff (make files, change files, do programming, etc.)
+# git add . adds everything in the current directory/folder OR git add code/pete/lab01.py add just one file OR git add -A adds everything in the repo
+# git commit -m "commit message!!" make the commit
+# git push push to github
+# When you’ve completed a lab, go to github and make a pull request to pull your branch into main
