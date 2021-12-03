ce1795af45006ef113b7f1ef450bb84f495febfc
LK-NightWalker <auriel.cofield@yahoo.com>
2021-12-03 18:33:25 -0500

    completed contact list lab

--- code/auriel/Python/contactlist_lab.py
+++ code/auriel/Python/contactlist_lab.py
@@ -1,6 +1,6 @@
 # Version 1 of lab
 contact_list = []
-file_path = r'code/auriel/Python/contact_list_auriel.csv'
+file_path = r'code/auriel/Python/contact_list_auriel copy.csv'
 
 with open(file_path, 'r') as csv_file:
     rows = csv_file.read().split('\n')
@@ -13,17 +13,16 @@ with open(file_path, 'r') as csv_file:
 
 # Version 2 and 3 of lab (Create a CRUD REPL)
 def create():
-    with open(file_path, 'a') as csv_file:
-        csv_file.write('\n')
-        fname = input('Please enter a first name for new record: ').lower()
-        csv_file.write(f'{fname}')
-        lname = input('Please enter a last name for new record: ').lower()
-        csv_file.write(f',{lname}')
-        city = input('Please enter a city for new record: ').lower()
-        csv_file.write(f',{city}')
+    fname = input('Please enter a first name for new record: ').lower()
+    lname = input('Please enter a last name for new record: ').lower()
+    city = input('Please enter a city for new record: ').lower()
+
+    new_entry = {headers[0]: fname, headers[1]: lname, headers[2]: city}
+    contact_list.append(new_entry)
 
     print(f'''The following contact has been added: \n
     firstname: {fname} lastname: {lname} city: {city}''')
+
 
 def retrieve():
     ls = []
@@ -48,26 +47,24 @@ def update():
     update_lname = input('Please enter last name of the record you want to update: ').lower()
     for x in contact_list:
         if update_fname == x['first_name'] and update_lname == x['last_name']:
-            update = input('What information would you like to update? \nFirst name, last name or city: ').lower()
+            new_information = input('What information would you like to update? \nFirst name, last name or city: ').lower()
             
-        if update == 'first name':
-            new_fname = input('What is the new first name?: ').lower()
-            x['first_name'] = new_fname
-        if update == 'last name':
-            new_lname = input('What is the new last name?: ').lower()
-            x['last_name'] = new_lname
-        if update == 'city':
-            new_city = input('What is the new city?: ').lower()
-            x['city'] = new_city
+            if new_information  == 'first name' or new_information == 'firstname':
+                new_fname = input('What is the new first name?: ').lower()
+                x['first_name'] = new_fname
+            if new_information  == 'last name' or new_information == 'lastname':
+                new_lname = input('What is the new last name?: ').lower()
+                x['last_name'] = new_lname
+            if new_information  == 'city' or new_information == 'city':
+                new_city = input('What is the new city?: ').lower()
+                x['city'] = new_city
 
 def delete():
     delete_fname = input('Please enter first name of the record you want to delete: ').lower()
     delete_lname = input('Please enter last name of the record you want to delete: ').lower()
-    for x in contact_list:
-        if delete_fname == x['first_name'] and delete_lname == x['last_name']:
-            del x['first_name']
-            del x['last_name']
-            del x['city']            
+    for c, person in enumerate(contact_list):
+        if delete_fname == person['first_name'] and delete_lname == person['last_name']:
+            del contact_list[c]
 
 while True:
     user = input('''
@@ -81,14 +78,24 @@ If you would like to quit, please type 'quit' ''').lower()
     elif user == 'create':
        create()
         
-    elif user == 'retrieve' or user == 'Retrieve':
+    elif user == 'retrieve':
         retrieve()
 
-    elif user == 'update' or user == 'Update':
-        create()
+    elif user == 'update':
+        update()
 
-    elif user == 'delete' or user == 'Delete':
+    elif user == 'delete':
         delete()
 
     else:
         print('Please enter one of the options listed')
+
+with open(file_path, 'w') as file:
+    file.write('first_name,last_name,city')
+    for x in contact_list:
+        file.write('\n')
+        file.write(x['first_name'])
+        file.write(',')
+        file.write(x['last_name'])
+        file.write(',')
+        file.write(x['city'])
