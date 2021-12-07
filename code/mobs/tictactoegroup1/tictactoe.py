8a08b03fd399e95911e4fc4be844aba049a532b3
Mastamint <Alnardo.barisic@gmail.com>
2021-12-07 12:30:33 -0800

    In Work

--- code/mobs/tictactoegroup1/tictactoe.py
+++ code/mobs/tictactoegroup1/tictactoe.py
@@ -14,32 +14,42 @@ class Player:
         
         self.player = player
         self.token = token
+
+    def __str__(self):
+        return f"{self.player} is {self.token}."
         
+
+# player1 = Player('Auriel', 'X')
 
 
 class Game:
 
     def __init__(self):
+        self.board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
-        self.board = board
-
-        # self.board = { 1: “ “, “2”: “ “, “3”: “ “,
-        #               “4”: “ “, “5”: “ “, “6”: “ “,
-        #               “7”: “ “, “8”: “ “, “9”: “ “}
-        ...        
+    def __str__(self):
+        return   f"{self.board[0]} + '|' + {self.board[1]} + '|' + {self.board[2]}\n{self.board[3]} + '|' + {self.board[4]} + '|' + {self.board[5]}\n{self.board[6]} + '|' + {self.board[7]} + '|' + {self.board[8]}"
 
 
-    def __str__(self, board):
+    def move(self, player, token):
+        moves = {'top left'       :  0,
+                 'top middle'     :  1,
+                 'top right'      :  2,
+                 'left middle'    :  3,
+                 'middle'         :  4,
+                 'right middle'   :  5,
+                 'bottom left'    :  6,
+                 'bottom middle'  :  7,
+                 'bottom right'   :  8,
+        }
 
-        board = [
-                ['', '|', '', '|', ''],
-                ['', '|', '', '|', ''],
-                ['', '|', '', '|', '']
-                ]
-    
-
-    def move(x, y, player):
-        ...
+        user_move = input(f"Choose an empty spot to place your token: ")
+        user_move = moves[user_move]
+        self.board[user_move] = token
+        print(self.board[0] + '|' + self.board[1] + '|' + self.board[2])
+        print(self.board[3] + '|' + self.board[4] + '|' + self.board[5])
+        print(self.board[6] + '|' + self.board[7] + '|' + self.board[8])
+        return self.board
 
 
     def calc_winner():
@@ -55,12 +65,8 @@ class Game:
 
 
 
-
-
-
-
-#player_o = Player("O", "o")
-
+test = Game()
+# print(test)
 
 
 while True: # ask for player 1's name and preferred token
@@ -78,15 +84,22 @@ while True: # ask for player 1's name and preferred token
 
     if player1_token == "X":
         
-        #player_x = Player("X", "x")
-
         player2_token = "O"
-
-        print("You  ")
-
         break
-        
 
+    if player1_token == "O":
+        player2_token = "X"
+        break
+
+
+player_1 = Player(player1_name, player1_token)
+player_2 = Player(player2_name, player2_token)
+
+test.move(player1_name, player1_token)
+
+
+# print(player_1)
+# print(player_2)
 
 
 
