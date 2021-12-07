c9dac10ea4280f86e1de6bd17378ce8bf56a34f7
travisjackson3 <travisjackson3@gmail.com>
2021-12-07 13:28:48 -0800

    update

--- code/mobs/tictactoegroup1/tictactoe.py
+++ code/mobs/tictactoegroup1/tictactoe.py
@@ -7,7 +7,6 @@ Date:
 
 
 
-
 class Player:
 
     def __init__(self, player, token):
@@ -28,10 +27,10 @@ class Game:
         self.board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
     def __str__(self):
-        return   f"{self.board[0]} + '|' + {self.board[1]} + '|' + {self.board[2]}\n{self.board[3]} + '|' + {self.board[4]} + '|' + {self.board[5]}\n{self.board[6]} + '|' + {self.board[7]} + '|' + {self.board[8]}"
+        return   f"{self.board[0]}  |  {self.board[1]}  |  {self.board[2]}\n{self.board[3]}  |  {self.board[4]}  |  {self.board[5]}\n{self.board[6]}  |  {self.board[7]}  |  {self.board[8]}"
 
 
-    def move(self, player, token):
+    def move(self, player, token, user_move):
         moves = {'top left'       :  0,
                  'top middle'     :  1,
                  'top right'      :  2,
@@ -43,13 +42,9 @@ class Game:
                  'bottom right'   :  8,
         }
 
-        user_move = input(f"Choose an empty spot to place your token: ")
         user_move = moves[user_move]
         self.board[user_move] = token
-        print(self.board[0] + '|' + self.board[1] + '|' + self.board[2])
-        print(self.board[3] + '|' + self.board[4] + '|' + self.board[5])
-        print(self.board[6] + '|' + self.board[7] + '|' + self.board[8])
-        return self.board
+
 
 
     def calc_winner():
@@ -61,12 +56,14 @@ class Game:
 
 
     def is_game_over():
-        ...
+        return True
+
+game_over = False
 
 
 
-test = Game()
-# print(test)
+
+
 
 
 while True: # ask for player 1's name and preferred token
@@ -92,15 +89,31 @@ while True: # ask for player 1's name and preferred token
         break
 
 
+
 player_1 = Player(player1_name, player1_token)
 player_2 = Player(player2_name, player2_token)
 
-test.move(player1_name, player1_token)
+
+turn = 0
+new_board = Game()
+
+while game_over == False:
+
+    if turn % 2 == 0:
+        user_move = input(f"{player1_name}: choose an empty spot to place your token: ")
+
+        new_board.move(player1_name, player1_token, user_move)
+        turn += 1
+    
+    elif turn % 2 == 1:
+        user_move = input(f"{player2_name}: choose an empty spot to place your token: ")
+        new_board.move(player2_name, player2_token, user_move)
+        turn += 1
+
+    print(new_board)
 
 
-# print(player_1)
-# print(player_2)
-
+    # check is_game_over()
 
 
 
