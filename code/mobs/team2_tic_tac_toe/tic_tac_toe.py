c3326cbef973221ddf5703a3805f515ef53960db
lisab1993 <brown.lisa999@gmail.com>
2021-12-08 10:03:23 -0800

    started tic-tac-toe

---
+++ code/mobs/team2_tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, name='', token=''):
+        self.name=name
+        self.token=token
+
+class Game:
+    def __init__(self, board=[]):
+        self.board = board
+
+    def __str__():
+        '''Returns a pretty string representation of the game board'''
+
+    def move(x, y, player):
+        '''Moves a token through the x an y axes'''
+
+    def calc_winner():
+        '''Determines which player has won the game'''
+
+    def is_full():
+        '''Determines if the board is full with no winner'''
+
+    def is_game_over():
+        '''Returns true if the board is full or a player has won'''
+
