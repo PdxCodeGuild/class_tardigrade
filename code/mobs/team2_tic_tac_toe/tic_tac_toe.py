3c9cd87752e0ecf0557bc7f9111e61dea81cafe7
christian <chrisalv27@yahoo.com>
2021-12-08 11:49:51 -0800

    tic tac toe

--- code/mobs/team2_tic_tac_toe/tic_tac_toe.py
+++ code/mobs/team2_tic_tac_toe/tic_tac_toe.py
@@ -2,46 +2,51 @@ class Player:
     def __init__(self, name='', token=''):
         self.name=name
         self.token=token
+    def __repr__(self):
+        return f'Player("{self.name}","{self.token}")'
 
 class Game:
-    def __init__(self, board=[], moves=0):
-        self.board = board
+    def __init__(self, moves=0):
+        
         self.moves = moves
+        self.board = {1: " ",
+                2: " ",
+                3: " ",
+                 4: " ",
+                  5: " ",
+                  6: " ",
+                    7: " ",
+                    8: " ",
+                    9: " "}
 
     def __str__(self):
         '''Returns a pretty string representation of the game board'''
-        top = {1: " ",
-                2: " ",
-                3: " "}
-        middle = {4: " ",
-                  5: " ",
-                  6: " "}
-        bottom = {7: " ",
-                    8: " ",
-                    9: " "}
-        self.board.append(top)
-        self.board.append(middle)
-        self.board.append(bottom)
-        print(top[1] + '|' + top[2] + '|' + top[3])
-        print('-+-+-')
-        print(middle[4] + '|' + middle[5] + '|' + middle[6])
-        print('-+-+-')
-        print(bottom[7] + '|' + bottom[8] + '|' + bottom[9])
+       
+        b = self.board
+        line1 = b[1] + '|' + b[2] + '|' + b[3]
+        line2 = '-+-+-'
+        line3 = b[4] + '|' + b[5] + '|' + b[6]
+        line4 = '-+-+-'
+        line5 = b[7] + '|' + b[8] + '|' + b[9]
+        return line1 + '\n' + line2 + '\n' + line3 + '\n' + line4 + '\n' + line5
+    def move(self,command, player):
+        b = self.board
+        return command
 
-    def move(self,x, y, player):
         '''Moves a token through the x an y axes'''
     
         self.moves += 1
         if self.moves >= 9:
             self.is_full()
 
-    def calc_winner():
+    def calc_winner(self):
         '''Determines which player has won the game'''
 
-    def is_full():
+    def is_full(self):
         '''Determines if the board is full with no winner'''
        
-    def is_game_over():
+    def is_game_over(self):
+        return False
         '''Returns true if the board is full or a player has won'''
 
 
@@ -49,10 +54,13 @@ class Game:
 
 game = Game()
 def main():
-    while True:
-        print("Welcome to tic-tac-toe!")
-        player1=Player(input("X what is your name? "), "X")
-        player2=Player(input("O What is your name? "), "O")
-        command= input("What is your move? ")
-
-print(game)
+    print("Welcome to tic-tac-toe!")
+    player1=Player(input("X what is your name? "), "X")
+    player2=Player(input("O What is your name? "), "O")
+    print(game)
+    # while not game.is_game_over() == True:
+    #     print(game)
+    #     command= input("What is your move? ")
+    command = input(f'enter a number position (1-9) to place your token: ')
+    print(game.move)
+main()
