a743c691ae728fe4b3231f3b00013a3c7878f96e
travisjackson3 <travisjackson3@gmail.com>
2021-12-09 14:06:02 -0800

    Lab16

--- code/travis/Lab16.py
+++ code/travis/Lab16.py
@@ -5,3 +5,43 @@ Date: 12/9/21
 
 """
 
+import requests
+import time
+import re
+
+response = requests.get("https://icanhazdadjoke.com/", headers = {"accept": "application/json"})
+data = response.json()
+
+
+joke = data["joke"]
+
+#splits joke at puncuation to capture a punchline
+suspense = re.split("(?<=[\.\?\!])", joke )
+
+
+# prints 
+for item in suspense:
+
+    if item == "" or item == "." or item == " ":
+        
+        continue
+
+    else:
+
+        print(item.lstrip())
+        time.sleep(3)
+
+#part 2
+
+#ask to search
+#repl
+
+while True:
+
+    search_joke = requests.get("https://icanhazdadjoke.com/search", 
+    ...
+
+
+
+
+
