063e1c8167a7ac0ca5f6f511f19fcaa7f43f2548
LK-NightWalker <auriel.cofield@yahoo.com>
2021-12-10 16:49:39 -0500

    finished quotes API

--- code/auriel/Python/quotes_lab.py
+++ code/auriel/Python/quotes_lab.py
@@ -10,25 +10,34 @@ print(data['quote']['body'])
 
 # Version 2 of Lab
 while True:
-    user_input = input("Enter a keyword to search for quotes or enter 'done' to quit: ").lower()
+    user_input = input("Enter a keyword to search for quotes or 'done' to quit: ").lower()
 
+    page = 1
     if user_input == 'done':
         break
-    else: 
-        page = 1
+    while True:
+        
         num_quotes = 0
         keyword_url = f'https://favqs.com/api/quotes?page={page}&filter={user_input}'
         headers = {
-        'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'
-    }
+            'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'
+        }
         response = requests.get(keyword_url, headers = headers)
         data = response.json()
 
         for i in range(0, len(data['quotes'])):
             num_quotes += 1
-        print(f'{num_quotes} quotes associated with {user_input} - page {page}')
+        print(f'{num_quotes} quotes associated with {user_input} - page {page}\n')
         
         for quote in data['quotes']:
             print(quote['body'])
-        
+
         next_page = input("Enter 'next page' or 'done': ")
+
+        if next_page == 'next page' and data['last_page'] == True or next_page == 'next' and data['last_page'] == True:
+            print('There are no more pages\n')
+            break
+        elif next_page == 'next page' or next_page == 'next':
+            page += 1
+        elif next_page == 'done':
+            break
