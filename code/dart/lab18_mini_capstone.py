35fad1273fa26b02f54bf5b4ab19771d54d580ef
d'Artagnan Childers <dartagnanchilders@dArtagnans-MacBook-Pro.local>
2021-12-15 15:34:36 -0800

    lab18_mini_capstone.py

--- code/dart/lab18_mini_capstone.py
+++ code/dart/lab18_mini_capstone.py
@@ -1,95 +1,60 @@
-# create a discord bot using python
-import discord
-import requests
-import json
+# Bot is short for robot and is a software application that performs automated, repetitive and pre-defined tasks. 
+# Bots typically imitate or replace human behavior. As they are automated, they operate much faster than human users.
+
+import discord # allows us to communicate with the discord server
 import random
-from replit import db
-from keep_alive import keep_alive
+import time
 
-client = discord.Client()
-
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
-
-starter_encouragements = [
-  "Cheer up! You're about of a great team known as the Death Eaters and together we will destroy Harry Potter!",
-  "Hang in there.",
-  "You are a great person!"
-]
-
-if "responding" not in db.keys():
-  db["responding"] = True
-
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
-
-def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
-
-def delete_encouragment(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-    db["encouragements"] = encouragements
+intents = discord.Intents.default()
+intents.members=True
+client = discord.Client(intents = intents) # discord.Client() creates a connection to Discord. This class is used to interact with Discord.
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print("Bot is ready and logged in as {0.user}".format(client))
+
+@client.event
+async def on_member_join(member):
+  channel = client.get_channel(920409186674819086)
+  # embed = discord.Embed(title = f"Yo what's up {member.name}!", description = f"Thanks for joining {member.guild.name}. 
+  # I'm Lord V, formally known as Tom Riddle, you may have heard of me. Ready to defeat Harry Potter once and for all?")
+  # embed.set_thumbnail(url = member.avatar_url) # Set the embed's thumbnail to the member's avatar image!
+  welcome_messages = [
+    f"Welcome, {member.name}! Together we're going to destory Harry Potter!",
+    f"What's going on {member.name}?, I heard you hate Harry Potter just as much as I do. I think you'll be a valuable asset to the team.",
+    f"Yo what's up {member.name}. I'm Lord V, but you can call me Tom. How're your wand skills?",
+  ]
+
+  time.sleep(1)
+  random_message = random.choice(welcome_messages)
+
+  await channel.send(random_message)
+  # await channel.send(embed = embed)
+
+  if not channel:
+    return
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
+  username = str(message.author).split("#")[0]
+  user_message = str(message.content)
+  channel = str(message.channel.name)
+  print(f"{username}: {user_message} ({channel})")
+
+  if message.author == client.user: # if message author == bot, it will not reply to itself 
     return
 
-  msg = message.content
+  if message.channel.name == "general":
+    if user_message == "Hello".lower() or user_message == "Hey".lower() or user_message == "Whats up".lower() or user_message == "What's up".lower() or user_message == "Sup".lower():
+      await message.channel.send(f"Hello {username}. I'm glad to have you on our team. What do you say we destroy Harry Potter")
+      return
+    elif user_message == "Bye".lower() or user_message == "see you later".lower():
+      await message.channel.send(f"See you later {username}. Keep practicing your wand skills.")
+    elif user_message == "Harry Potter".lower():
+      await message.channel.send(f"Did someone say Harry Potter? Ugh, I hate that guy.")
+    elif any(word in user_message for word in "Harry") or any(word in user_message for word in "Hairy"):
+      await message.channel.send(f"Did someone say Harry? As in Harry Potter? Yeah, we don't talk about him here.")
+    elif any(word in user_message for word in "Potter"):
+      await message.channel.send(f"Did you say Potter? If you're talking about Harry Potter, STOP.")
 
-  if msg.startswith('$inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
-
-  if db["responding"]:
-    options = starter_encouragements
-    if "encouragements" in db.keys():
-      options = options + db["encouragements"]
-
-    if any(word in msg for word in sad_words):
-      await message.channel.send(random.choice(options))
-
-  if msg.startswith("$new"):
-    encouraging_message = msg.split("$new ",1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("New encouraging message added.")
-
-  if msg.startswith("$del"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      index = int(msg.split("$del",1)[1])
-      delete_encouragment(index)
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
-
-  if msg.startswith("$list"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
-
-  if msg.startswith("$responding"):
-    value = msg.split("$responding ",1)[1]
-
-    if value.lower() == "true":
-      db["responding"] = True
-      await message.channel.send("Responding is on.")
-    else:
-      db["responding"] = False
-      await message.channel.send("Responding is off.")
-
-keep_alive()
-client.run("OTIwNDA5NTM2MzU4MTI1NTY4.Ybj8LQ.vvculqtxtheAHuG0TtYUSZE9fQM")
+client.run("OTIwNDA5NTM2MzU4MTI1NTY4.Ybj8LQ.ZQ8tqZxoVioA55HeNLEdxFOrrHY") # runs the bot when we run the file
