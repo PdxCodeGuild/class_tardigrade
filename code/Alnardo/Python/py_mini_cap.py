7b62108ae05191d4b925cf52c650f489a67aaf3b
Mastamint <Alnardo.barisic@gmail.com>
2021-12-14 16:25:36 -0800

    Updated mini cap

--- code/Alnardo/Python/py_mini_cap.py
+++ code/Alnardo/Python/py_mini_cap.py
@@ -2,38 +2,59 @@
 #  Plugging in an equation and getting its graph back
 #  numpy 1.21.4 installed in C:\Users\Alnar\AppData\Local\Programs\Python\Python310\Scripts
 #  MATPLOTLIB  cycler-0.11.0 fonttools-4.28.3 kiwisolver-1.3.2 matplotlib-3.5.0 packaging-21.3 pillow-8.4.0 pyparsing-3.0.6 python-dateutil-2.8.2 setuptools-scm-6.3.2 six-1.16.0 tomli-1.2.2 installed
+
+
 import matplotlib.pyplot as plt
-x=range(0,9)
+x1_min = int(input('Please enter the minimum value for x: '))
+x2_max = int(input('Please enter the maximum vlaue for x: '))
+
+x=range(x1_min, x2_max+1)
+
 y=[]
+
 for i in x:
     y.append(i**2)
 mean = 0
 counter = 0
-for j in y:
-    # print(j)
-    mean += j
-    counter += 1
-# print(range(len(y)))
-print(counter)
-median = 0
-if counter % 2 == 0:
-    median = int(counter/2)
-    median = y[median]
-    print(median)
-if counter % 2 == 1:
-    x1 = counter // 2
-    x2 = (counter // 2) + 1
-    median = (y[x1] + y[x2]) / 2
-    print(median)
+question = input('Would you like to calculate the median and/or mean? (yes/no): ')
+while question == 'yes':
+    choice = input('Would you like the mean?: ')
+    for j in y:
+        mean += j
+        counter += 1
+    if choice == 'yes':
+        average = mean / counter
+        print(average)
+    choice == input('Would you like the median?: ')
+    if choice == 'yes':
+        median = 0
+        if counter % 2 == 0:
+            median = int(counter/2)
+            median = y[median]
+            print(median)
+        elif counter % 2 == 1:
+            x1 = counter // 2
+            x2 = (counter // 2) + 1
+            median = (y[x1] + y[x2]) / 2
+            print(median)
+    break
 
 
-# plt.plot(x, y)
-# plt.plot([-2, 2, 3, 4], [1, 4, 9, 21]) #.plot ([x-coordinates], [y-coordinates])
-# plt.axis([0, 2, -1, 10]) # .axis([x starting point, x ending point, y starting point, y ending point])
-# plt.xlabel(), plt.ylabel()   #Labeling X and Y axis
-# plt.plot([-2, 2, 3, 4], [1, 4, 9, 21], color='green', marker='x', linestyle='dashed', linewidth=2, markersize=12) # (x-coordinates, y-coordinates, line-color, how each plot is marked)
-# plt.legend() #  Shows legend in graph
-# plt.title() #  Adds a title to the top of the graph
+marker_options = ['.', ',', 'o', 'v', '^', 'x', '<', '>', '1', '2', '3', '4', '8', 's', 'p', 'P', 'd', 'D', 'X']
+while True:
+    marker_choice = input('What kind of marker would you like to use? (Enter "options" for a list of available choices): ')
+    if marker_choice == 'options':
+        print(marker_options)
+    elif marker_choice not in marker_options:
+        print('Invalid choice')
+    else:
+        break
+
+print(marker_choice)
+
+plt.plot(x, y, color='red', marker=marker_choice, linestyle='dashed', linewidth=1, markersize=10) 
+
+plt.title('Test graph')
 
 
-# plt.show()
+plt.show()
