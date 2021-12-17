a971ab88310563c5ca495a07055388ec47622d6c
Vanessa <v.d.chacon@gmail.com>
2021-12-16 16:21:19 -0800

    end of day

--- code/vanessa/minicap.py
+++ code/vanessa/minicap.py
@@ -78,7 +78,7 @@ def congrats (message='Congratulations! Keep up the great work!'):
     return total
 
 
-schedule.every(1).minutes.do(goal1)
+schedule.every(.25).minutes.do(goal1)
 schedule.every(5).to(10).minutes.do(goal5)
 schedule.every(5).to(10).days.do(goal1)
 schedule.every(1).hour.do(goal2, )
@@ -89,29 +89,32 @@ schedule.every().day.at("13:47").do(goal4)
 schedule.every().day.at("13:48:30").do(congrats)
 
 
-# for i in range(120,0,-1):
-#      time.sleep(1)
-#      schedule.run_pending()  
-
-final= (len(counter))-1
-print(final)
-
-progress= input("do you want to see your progress? (y/n)")
-number_of_days= [0,1,2,3,4,5,6,7]
-final_counts=[4,5,6,5,5,10]
-final_counts.append(final)
 
 def success():
     w= figure(plot_width=400, plot_height=400, title="Goal Tracker", x_axis_label = "days", y_axis_label= "goals met")
     w.star(number_of_days, final_counts, size=15, line_color="navy", fill_color="orange", fill_alpha=0.5)
     show(w)
 
+number_of_days= [0]
+final_counts=[0]
 
-if progress == "y":
-   (success())
+while True:
+   number_of_days.append(len(number_of_days) + 1)
+   for i in range(100,0,-1): #really should be 86400 seconds for an entire days worth of input.
+        time.sleep(1)
+        schedule.run_pending()
+   final= (len(counter))-1
+   print(final)
+   final_counts.append(final)
+   print(final_counts)
+   progress= input("do you want to see your progress? (y/n)")
+   if progress == "y":
+        (success())
+   time.sleep(60)
 
 
 
 #end of day, add to input list, each number = each day. use input list in graph for 10 days/30 days
 #maybe put final graph in seperate function or build graph day by day? 
+#make x input dynamic based on final counts entered
 
