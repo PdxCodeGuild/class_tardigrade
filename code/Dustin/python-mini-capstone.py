a22fedb9c26c18623ed4ed80b4460ce8872b45c4
Dustin <94391156+IronWill12@users.noreply.github.com>
2021-12-16 22:34:44 -0700

    working on capstone, still trying to get text to work

--- code/Dustin/python-mini-capstone.py
+++ code/Dustin/python-mini-capstone.py
@@ -4,13 +4,38 @@
  The program should notify the user when the Crypto is at their buy or sale price.
  """
 import cryptocompare
+import twilio
+
 print("Welcome to the Crypto Currency Buy-And-Sale-A-Tron")
 coin = input("Please enter the ticker symbol for the Crypto you are interested in: ")
+
+
 
 buy_price = input("At what price would you like to buy? ")
 sale_price = input("At what price would you like to sale? ")
 
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+buy_message = client.messages \
+    .create(
+         body=f'The {coin} has reached your specified buying price of {buy_price}.',
+         from_='+15017122661',
+         to='+18016902294'
+     )
+
+sale_message = client.messages \
+    .create(
+         body=f'The {coin} has reached your specified buying price of {sale_price}.',
+         from_='+15017122661',
+         to='+18016902294'
+     )
 price = cryptocompare.get_price('DOGE', 'USD')
 
 print(price)
-
+if buy_price >= price:
+    buy_message
+if sale_price <= price:
+    sale_message
