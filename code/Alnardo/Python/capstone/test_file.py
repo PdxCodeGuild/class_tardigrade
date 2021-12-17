9df6e6bd64fa18b9cd62712a6bea66869726d790
Mastamint <Alnardo.barisic@gmail.com>
2021-12-16 16:29:30 -0800

    Day 3 update

--- code/Alnardo/Python/capstone/test_file.py
+++ code/Alnardo/Python/capstone/test_file.py
@@ -20,9 +20,21 @@ import numpy as np
 # print(y)
 
     # y.append((i+2)+3)
-num_1 = int(input('Please enter the minimum number: '))
-num_2 = int(input('Please enter the maximum number: '))
+
+tan = np.tan
+sin = np.sin
+cos = np.cos
+
+# x = 0
+# print(answer)
+# sohcahtoa = input("Please enter sin, cos, or tan?: ")
+# answer_2 = getattr(np, sohcahtoa)
+num_1 = int(input('Please enter the minimum number for x: '))
+num_2 = int(input('Please enter the maximum number for x: '))
 x = np.arange(num_1*np.pi,num_2*np.pi,0.1)   # start,stop,step
-y = np.tan(x)
-plt.plot(x, y)
+test = input('Enter your trig equation: ')
+answer = eval(test)
+y = answer
+z = np.sin(x)
+plt.plot(x, y, x, z)
 plt.show()
