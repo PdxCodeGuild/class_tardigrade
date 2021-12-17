18e429b4353517563dba233ec40c2523ba264720
Shad-Wi <94399458+Shad-Wi@users.noreply.github.com>
2021-12-17 15:16:59 -0500

    capstone

--- code/Shad/capstone.py
+++ code/Shad/capstone.py
@@ -3,8 +3,13 @@ import webbrowser
 import os
 import folium
 import geocoder
+import geojson
+import requests
+import openrouteservice
+import pandas
+import secrets
 
-g = geocoder.arcgis(input('city:   '))
+g = geocoder.arcgis('Enter a start city:')
 m = folium.Map(location=[g.lat, g.lng], zoom_start=15)
 # folium.Marker(
 #    [45.3288, -121.6625], popup="<i>Mt. Hood Meadows</i>", tooltip='hello'
@@ -33,7 +38,7 @@ def menu():
         elif choice == 2:
             pins()
         elif choice == 3:
-            route()
+            routes()
         else:
             print("Please try again")
             menu()
@@ -43,9 +48,11 @@ def menu():
 def place():
     
     g = geocoder.arcgis(input('Enter a city:   '))
-    # m = folium.Map(location=[g.lat, g.lng], zoom_start=12)
-    m.save("index.html")
-    webbrowser.open('index.html')
+    
+    m = folium.Map(location=[g.lat, g.lng], zoom_start=12)
+    m.save("dex.html")
+    webbrowser.open('dex.html')
+    
 
 
 def pins():
@@ -58,22 +65,45 @@ def pins():
     Counter+=1
     folium.Marker([lati, lon], tooltip='{Counter}').add_to(m)
 
-    m.save("index.html")
+    m.save("dex.html")
+    return p
+def routes():
+        city = input('city:   ')
+        g = geocoder.arcgis(city)
+        g2 = geocoder.arcgis(input('city:   '))
+        
+        client = openrouteservice.Client(key=secrets.secret_key)
+        # coordinates =[(g.lat, g.lng), (g2.lat, g2.lng)]
+        coordinates =[ (g.lng, g.lat), (g2.lng, g2.lat)]
+
+        
+        
+
+        m = folium.Map(location=[g.lat, g.lng], zoom_start=15)
+        folium.Marker([g.lat,g.lng], popup=g, tooltip=g).add_to(m)
+        folium.Marker([g2.lat,g2.lng], popup=g2, tooltip=g2).add_to(m)
+        # folium.PolyLine([(g.lat,g.lng),(g2.lat,g2.lng)],
+        #                     color='red',
+        #                     weight=1,
+        #                     opacity=1).add_to(m)
 
 
-def route():
-    p = geocoder.arcgis(input('Enter a pin location:   '))
-    lati =p.lat
-    lon= p.lng
+        route = client.directions(coordinates=coordinates, profile='driving-car', format='geojson')
+        # One of [“driving-car”, “driving-hgv”, “foot-walking”, “foot-hiking”, “cycling-regular”, “cycling-road”,”cycling-mountain”, “cycling-electric”,]. Default “driving-car”.
+        folium.GeoJson(route, name='route').add_to(m)
+        folium.Marker([g.lat,g.lng], popup=g, tooltip=g).add_to(m)
+        folium.Marker([g2.lat,g2.lng], popup=g2, tooltip=g2).add_to(m)
+        
+        print(route['features'][0]['properties']['segments'][0]['distance']*0.000621371, 'miles')
+        print(route['features'][0]['properties']['segments'][0]['duration']*0.000277778, 'hours\n')
 
-    loc = [(lati, lon),
-       (lati, lon)]
+        m.save("dex.html")
 
-    folium.PolyLine(loc,
-                    color='red',
-                    weight=12,
-                    opacity=1).add_to(m)
-    m.save("index.html")
+
+        webbrowser.open('dex.html')
+        
+
+
 
 
 main()
