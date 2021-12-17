25cc7d46635c5ea98aa98906c94aaac72419c4a4
Vanessa <v.d.chacon@gmail.com>
2021-12-17 10:56:54 -0800

    presentation

--- code/vanessa/minicap.py
+++ code/vanessa/minicap.py
@@ -79,14 +79,14 @@ def congrats (message='Congratulations! Keep up the great work!'):
 
 
 schedule.every(.25).minutes.do(goal1)
-schedule.every(5).to(10).minutes.do(goal5)
-schedule.every(5).to(10).days.do(goal1)
+schedule.every(1).to(2).minutes.do(goal5)
+schedule.every(3).to(6).days.do(goal1)
 schedule.every(1).hour.do(goal2, )
-schedule.every().day.at("13:45").do(goal2)
-schedule.every().day.at("13:46:30").do(goal3)
-schedule.every().day.at("13:47").do(goal4)
+schedule.every().day.at("10:41").do(goal2)
+schedule.every().day.at("10:42:30").do(goal3)
+schedule.every().day.at("10:43").do(goal4)
 # schedule.every().day.at("14:78").do(daily_affirmation)
-schedule.every().day.at("13:48:30").do(congrats)
+schedule.every().day.at("10:43:30").do(congrats)
 
 
 
@@ -95,12 +95,12 @@ def success():
     w.star(number_of_days, final_counts, size=15, line_color="navy", fill_color="orange", fill_alpha=0.5)
     show(w)
 
-number_of_days= [0]
-final_counts=[0]
+number_of_days= [1,2,3]
+final_counts=[1,5,8]
 
 while True:
    number_of_days.append(len(number_of_days) + 1)
-   for i in range(100,0,-1): #really should be 86400 seconds for an entire days worth of input.
+   for i in range(120,0,-1): #really should be 86400 seconds for an entire days worth of input.
         time.sleep(1)
         schedule.run_pending()
    final= (len(counter))-1
