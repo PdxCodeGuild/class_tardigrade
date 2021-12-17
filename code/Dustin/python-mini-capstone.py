ba2fc1f171c02c8c4051c4ab2c57c47edaa529b9
Dustin <94391156+IronWill12@users.noreply.github.com>
2021-12-17 14:54:11 -0700

    on the right branch now, pushing for mini-cap and misc updates

--- code/Dustin/python-mini-capstone.py
+++ code/Dustin/python-mini-capstone.py
@@ -3,40 +3,59 @@
  Then the user can set a "buy" price and a sell price.
  The program should notify the user when the Crypto is at their buy or sale price.
  """
+#import required api
+
 import cryptocompare
-import twilio
+from twilio.rest import Client
+import time
+from secrets import token, sid
+#intro
 
 print("Welcome to the Crypto Currency Buy-And-Sale-A-Tron")
+
+#user input, also in the future could get phone number if I wanted others to use it.
+#for coin input need to do some safe guarding for people entering non valid options.
 coin = input("Please enter the ticker symbol for the Crypto you are interested in: ")
+coin = coin.upper()
+
+#also here need to put in some protections if someone enters a non int or float
+buy_price = float(input("At what price would you like to buy? "))
+sell_price = float(input("At what price would you like to sell? "))
 
 
-
-buy_price = input("At what price would you like to buy? ")
-sale_price = input("At what price would you like to sale? ")
-
-
-account_sid = "AC5ae2b44fa7ea31f4b18474bc353dca6c"
-# Your Auth Token from twilio.com/console
-auth_token  = "42b451bcb0d49f773160054605bdbbe5"
+account_sid = sid
+# Auth Token from twilio.com/console
+auth_token  = token
 client = Client(account_sid, auth_token)
 
-buy_message = client.messages \
-    .create(
-         body=f'The {coin} has reached your specified buying price of {buy_price}.',
-         from_='+12184408410',
-         to='+18016902294'
-     )
+price = cryptocompare.get_price( coin, 'USD')
+x = price[coin]['USD']
 
-sale_message = client.messages \
-    .create(
-         body=f'The {coin} has reached your specified buying price of {sale_price}.',
-         from_='+12184408410',
-         to='+18016902294'
-     )
-price = cryptocompare.get_price('DOGE', 'USD')
+# This section here will be for running the program unitl a text is sent then terminate.
 
-print(price)
-if buy_price >= price:
-    print(print(message.sid))
-if sale_price <= price:
-    print(sale_message.sid)
+def text_notify (condition, text): #defines function
+   text_sent = True
+    while text_sent == True:
+       if condition: #checks the condition
+            text
+           text_sent = False
+       time.sleep(600) #waits 10 min to run again
+
+
+#check for price and sends message
+#this needs to be timed and then terminated
+if buy_price >= x:
+    buy_message = client.messages \
+        .create(
+        body=f'The {coin} has reached your specified buying price of {buy_price}.',
+        from_='+12184408410',
+        to='+18016902294'
+    )
+
+if sell_price <= x:
+    sell_message = client.messages \
+        .create(
+        body=f'The {coin} has reached your specified sellinging price of {sell_price}.',
+        from_='+12184408410',
+        to='+18016902294'
+    )
