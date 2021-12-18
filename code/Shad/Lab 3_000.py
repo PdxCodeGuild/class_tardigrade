d9f27bf1511b705dda6f346eac347f547be35d98
Shad-Wi <94399458+Shad-Wi@users.noreply.github.com>
2021-12-17 20:26:40 -0500

    lab 3

--- code/Shad/Lab 3_000.py
+++ code/Shad/Lab 3_000.py
@@ -1,16 +1,22 @@
 
 amount= float(input('Enter a dollar amount: '))
 
-p = amount/.01
-Q = p//25
-d= Q//5
-d1 = d/10
+total_pennies = amount/.01
+
+quarters = total_pennies//25
+remaining_pennies= total_pennies%25
+
+dimes= remaining_pennies//10
+remaining_pennies= remaining_pennies%10
+
+nickels = remaining_pennies//5
+remaining_pennies= total_pennies%5
 
 
-
-print('dime',d)
-
-
-print(p, "pennies")
-print(Q, "quarter")
-
+# print(total_pennies, "pennies")
+# print(quarters, "quarter")
+# print(dimes, 'dimes')
+# print(nickels, 'nickels')
+# print(remaining_pennies, 'pennies')
+print(10%2)
+print(10%3)
