07537396583b378711c4c9e78a4f18a132acc1e7
pjz987 <pwj2012@gmail.com>
2022-01-04 12:28:37 -0800

    comments

--- code/pete/django/examples/emoticon_generator/emoticon_app/views.py
+++ code/pete/django/examples/emoticon_generator/emoticon_app/views.py
@@ -1,4 +1,3 @@
-from django import http
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from random import choice
@@ -11,8 +10,9 @@ def index(request):
     mouth = [')', '(', ']']
     emoticon = choice(eyes) + choice(nose) + choice(mouth)
 
-    saved_emoticons = Emoticon.objects.all()
+    saved_emoticons = Emoticon.objects.all() # Model.objects.all() returns a QuerySet of all with every Model from the database
     print(saved_emoticons)
+    # the context dictionary is how you pass variables into a template
     context = {
         'emoticon': emoticon,
         'saved_emoticons': saved_emoticons,
@@ -22,8 +22,10 @@ def index(request):
 
 def save(request, emoticon_str):
     Emoticon.objects.create(emoticon=emoticon_str)
+    # the redirect function can redirect to another view
     return redirect('/')
 
 def hello(request):
     messages = ['hello world', 'django is fun', 'django is not so fun']
+    # HttpResponse returns an instance of a reponse as a string
     return HttpResponse(choice(messages))
