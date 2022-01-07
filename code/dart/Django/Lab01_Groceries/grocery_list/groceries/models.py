3e5fe2e2572a9d53aff355999ac0eb17f154ad4f
d'Artagnan Childers <dartagnanchilders@dArtagnans-MacBook-Pro.local>
2022-01-07 15:12:21 -0800

    Lab01_Groceries

--- code/dart/Django/Lab01_Groceries/grocery_list/groceries/models.py
+++ code/dart/Django/Lab01_Groceries/grocery_list/groceries/models.py
@@ -1,25 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class GroceryItem(models.Model):
-    # class Meta: # used to make the plural name better in the admin panel (Citys vs Cities)
-    #     verbose_name_plural = 'cities'
 
-    description = models.CharField(max_length=100)
-    created_date = models.DateField(auto_now=True)
+    item = models.CharField(max_length=100)
+    created_date = models.DateField(auto_now_add=True)
     completed_date = models.DateField(null=True, blank=True)
-    completed = models.BooleanField()
-
-    # item = GroceryItem.objects.create()
-
-    # id = 1
-    # item = GroceryItem.objects.get(id=id)
-    # item.completed = True
-    # item.completed_date = timezone.now()
-    # item.save()
-
-
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name 
+        return self.item
--- code/dart/Django/Lab01_Groceries/grocery_list/grocery_list/settings.py
+++ code/dart/Django/Lab01_Groceries/grocery_list/grocery_list/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'groceries'
+    'groceries.apps.GroceriesConfig'
 ]
 
 MIDDLEWARE = [
