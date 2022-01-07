95d683ebfa0ac4ee8860b1f65cffdba42a5aaa23
travisjackson3 <travisjackson3@gmail.com>
2022-01-07 15:44:09 -0800

    lab1 progress

--- code/travis/Django/lab01/grocery_list/models.py
+++ code/travis/Django/lab01/grocery_list/models.py
@@ -6,9 +6,9 @@ from datetime import datetime
 
 class GroceryItem(models.Model):
     description = models.CharField(max_length=20)
-    created_date = models.DateField(auto_now_add=True)
-    completed_date = models.DateField(null=True)
-    completed = models.BooleanField(null=True)
+    created_date = models.DateField()
+    completed_date = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
     
     def __str__(self):
         return self.description
--- code/travis/Django/lab01/grocery_list/views.py
+++ code/travis/Django/lab01/grocery_list/views.py
@@ -1,5 +1,56 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import GroceryItem
+from datetime import datetime
+
+
+# def index(request):
+#     context = {GroceryItem.description: ..., GroceryItem.created_date: ..., GroceryItem.completed_date:..., GroceryItem.completed:...}
+#     return render(request, 'grocery_list/index.html', context)
 
 
 def index(request):
-    return HttpResponse("Hello, world")
+
+    grocery_item_list = GroceryItem.objects.all()
+    print(grocery_item_list)
+
+
+    # for g in grocery_item_list:
+
+    #     output = g.description
+    #     output2 = g.created_date.strftime('%m/%d/%y')
+
+    #     for g in grocery_item_list:
+
+    #         if g.completed_date is None:
+
+    #             output3 ="Not completed"        
+    #         else:
+
+    #             output3 = g.completed_date.strftime('%m/%d/%y')
+        
+    #     output4 = str(g.completed)
+
+    context = {'grocery_item_list': grocery_item_list}
+    
+    return render(request, 'grocery_list/index.html', context)
+    #return HttpResponse("item:" + output + " created date:" + output2  + "completed date:" + output3 + output4)
+
+
+
+""" 
+    output = ','.join([g.description for g in grocery_item_list])
+    output2 = ','.join([g.created_date.strftime('%m/%d/%y') for g in grocery_item_list])
+  
+    for g in grocery_item_list:
+
+        if g.completed_date is None:
+            print("none test")
+            output3 ="".join("Not completed")         
+        else:
+            output3 = "".join(g.completed_date.strftime('%m/%d/%y'))
+
+
+    output4 = ','.join([str(g.completed) for g in grocery_item_list]) """
+
+    
