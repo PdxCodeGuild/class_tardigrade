54dd464e02bce550c1894022fa43883e750d214b
travisjackson3 <travisjackson3@gmail.com>
2022-01-07 16:23:42 -0800

    lab1progress

--- code/travis/Django/lab01/grocery_list/models.py
+++ code/travis/Django/lab01/grocery_list/models.py
@@ -2,12 +2,10 @@ from django.db import models
 from datetime import datetime
 
 
-
-
 class GroceryItem(models.Model):
     description = models.CharField(max_length=20)
-    created_date = models.DateField()
-    completed_date = models.DateField(null=True, blank=True)
+    created_date = models.DateTimeField()
+    completed_date = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     
     def __str__(self):
