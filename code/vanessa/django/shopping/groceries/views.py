f67335a4b220b3060fbb0c33bef600582a00cc1e
Vanessa <v.d.chacon@gmail.com>
2022-01-07 16:24:45 -0800

    update

--- code/vanessa/django/shopping/groceries/views.py
+++ code/vanessa/django/shopping/groceries/views.py
@@ -1,3 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Item
+
+
 def index(request):
-    return HttpResponse('hello world!')
+    grocery_list = Item.objects.all()
+    context = {
+        "grocery_list" : grocery_list
+    }
+    return render(request, 'groceries/index.html', context)
