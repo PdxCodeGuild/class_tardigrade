e92d555bfd707ba1e8744cdcfb42b83454b93aa6
LK-NightWalker <auriel.cofield@yahoo.com>
2022-01-11 15:01:17 -0500

    Grocery List

--- code/auriel/3. Django/grocerylist/grocery_list/urls.py
+++ code/auriel/3. Django/grocerylist/grocery_list/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'grocery_list'
 urlpatterns = [
     path('', views.home, name='list'),
+    path('add/', views.add, name = 'add'),
 ]
--- code/auriel/3. Django/grocerylist/grocery_list/views.py
+++ code/auriel/3. Django/grocerylist/grocery_list/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import GroceryItem
-from django.utils import timezone
-
 
 def home(request):
     groceryitem = GroceryItem.objects.all()
     context = {'groceryitem': groceryitem}
     return render(request, 'grocery_list/groceryitems.html', context)
+
+
+def add(request):
+    item = request.POST.get('item')
+    GroceryItem.objects.create(description = item)
+    return redirect('/')
+
+def complete(request, id):
+    ...
+
+def delete(request, id):
+    ...
