038c1522fbb12a65b745e81b10654a064a2e89d1
pjz987 <pwj2012@gmail.com>
2022-01-12 10:28:39 -0800

    census manytomany example

--- code/pete/django/examples/census/census/urls.py
+++ code/pete/django/examples/census/census/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'census'
 urlpatterns = [
-    path('', views.index),
-    path('<slug:slugified_name>/', views.state),
+    path('', views.index, name='index'),
+    path('<slug:slugified_name>/', views.state, name='state'),
+    path('person/<int:id>/', views.person, name='person'),
 ]
