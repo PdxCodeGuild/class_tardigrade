357f4daf58ec3d010adb7c83c6492b7954855f1c
LK-NightWalker <auriel.cofield@yahoo.com>
2022-01-12 14:25:43 -0500

    Grocery List

--- code/auriel/3. Django/grocerylist/grocery_list/urls.py
+++ code/auriel/3. Django/grocerylist/grocery_list/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = 'grocery_list'
+
 urlpatterns = [
     path('', views.home, name='list'),
     path('add/', views.add, name = 'add'),
-    path('', views.complete, name = 'complete'),
-    path('', views.delete, name = 'delete'),
+    path('complete/<int:id>/', views.complete, name = 'complete'),
+    path('<int:id>/delete', views.delete, name = 'delete'),
 ]
--- code/auriel/3. Django/grocerylist/grocery_list/views.py
+++ code/auriel/3. Django/grocerylist/grocery_list/views.py
@@ -1,13 +1,15 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import GroceryItem
 from django.utils import timezone
+from django.urls import reverse
 
 
 def home(request):
     groceryitem = GroceryItem.objects.all()
     context = {'groceryitem': groceryitem}
-    return render(request, 'grocery_list/groceryitems.html', context)
 
+    return render(request, 'grocery_list/groceryitems.html', context)
 
 def add(request):
     item = request.POST.get('item')
@@ -23,5 +25,5 @@ def complete(request, id):
 
 def delete(request, id):
     item = get_object_or_404(GroceryItem, id=id)
-    item.delete()
+    item.delete()    
     return redirect('grocery_list/groceryitems.html')
