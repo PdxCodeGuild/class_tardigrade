bee2b988211a9c4c920811da8d69194caf227986
Mastamint <Alnardo.barisic@gmail.com>
2022-01-11 19:59:49 -0800

    Library WIP

---
+++ code/Alnardo/Django/Library/library/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+        
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    publish_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=CASCADE)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
