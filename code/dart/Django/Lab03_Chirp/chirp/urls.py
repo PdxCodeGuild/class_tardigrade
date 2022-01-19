35ec912eb1c5772231d913447ca08da6266fb00f
d'Artagnan Childers <dartagnanchilders@dArtagnans-MacBook-Pro.local>
2022-01-18 16:23:30 -0800

    Lab03_Chirp

--- code/dart/Django/Lab03_Chirp/chirp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chirp'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('post/<int:id>', views.post, name='post'),
-]
