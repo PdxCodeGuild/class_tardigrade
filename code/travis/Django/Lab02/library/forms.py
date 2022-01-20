5493d8ed227c0d0bbe650f217060286f0c540636
travisjackson3 <travisjackson3@gmail.com>
2022-01-20 11:37:34 -0800

    lab2

--- code/travis/Django/Lab02/library/forms.py
+++ code/travis/Django/Lab02/library/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import CheckoutBook 
 
 
-# Create your forms here.
 class CheckoutForm(forms.ModelForm):
-
-    class Meta:
+    user = forms.CharField(label=False)
+    
+    class Meta:        
         model = CheckoutBook
-       # fields = ('title', 'user', 'is_checked_out', 'time_stamp')
+
         fields = ('user',)
