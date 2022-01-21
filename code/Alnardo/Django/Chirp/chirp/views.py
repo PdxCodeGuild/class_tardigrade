9974187f14d8945553d74fc34228b3863dd2d23c
Mastamint <Alnardo.barisic@gmail.com>
2022-01-20 16:27:17 -0800

    Chirp WIP

--- code/Alnardo/Django/Chirp/chirp/views.py
+++ code/Alnardo/Django/Chirp/chirp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .models import Post
 # Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        posts = request.user.chirps.all()
+        context = {'posts' : posts}
+        return render(request, 'chirp/index.html', context)
+    return render(request, 'chirp/index.html')
+
+def add(request):
+    title_input = request.POST.get('title')
+    chirp_input = request.POST.get('chirp')
+    Post.objects.create(title=title_input, chirp=chirp_input, user=request.user)
+    return redirect('/')
