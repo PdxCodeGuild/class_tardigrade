29e7806587248d7c4a3ab854ec20c92e415d17ef
christian <chrisalv27@yahoo.com>
2022-01-27 15:45:37 -0800

    todolist

--- code/mobs/VCD-todo/todo.js
+++ code/mobs/VCD-todo/todo.js
@@ -1,10 +1,15 @@
 let toDoList=[]
 let toDoButton = document.querySelector('#list')
 let item = document.querySelector('#item')
+let thingsList = document.querySelector('#things-list')
 toDoButton.addEventListener('submit', function(event) {
 			event.preventDefault()
-            let item = item.value
-            toDoList.push(item)
+            let itemValue = item.value
+            toDoList.push(itemValue)
+            const listLi = document.createElement('li')
+            thingsList.appendChild(listLi)
+            console.log(toDoList)
+            listLi.innerText = itemValue
 
 })
-    console.log(toDoList)
+   
