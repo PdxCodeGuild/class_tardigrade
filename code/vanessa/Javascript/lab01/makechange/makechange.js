d317812aa7d67f2a80beba4a8dcd91a1f095db1c
Vanessa <v.d.chacon@gmail.com>
2022-01-27 12:38:39 -0800

    part a completed/all pick 3

--- code/vanessa/Javascript/lab01/makechange/makechange.js
+++ code/vanessa/Javascript/lab01/makechange/makechange.js
@@ -9,23 +9,15 @@ const nickels= Math.floor(dimeRemainder/5)
 const nickelRemainder= dimeRemainder % 5
 const pennies= Math.floor(nickelRemainder/1)
 const pennyRemainder= nickelRemainder % 1
-alert(inputChange)
-console.log("numberofquarters")
-console.log(quarters)
-console.log("remainder")
-console.log(quarterRemainder)
-console.log("numberofdimes")
-console.log(dimes)
-console.log("remainder")
-console.log(dimeRemainder)
-console.log("numberofnickels")
-console.log(nickels)
-console.log("remainder")
-console.log(nickelRemainder)
-console.log("numberofpennies")
-console.log(pennies)
-console.log("remainder")
-console.log(pennyRemainder)
 
+alert(`${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies`)
 
-// for 1.98 (198) should have 7 quarters(1.75), 2 dimes (.20=1.95), 0 nickels, three pennies (.03=1.98).....need to fix after quarters/carryover remainder of quarters, etc.//
+// console.log(`${quarters} quarters`)
+// console.log(`${quarterRemainder} remainder`)
+// console.log(`${dimes} dimes`)
+// console.log(`${dimeRemainder} remainder`)
+// console.log(`${nickels} nickels`)
+// console.log(`${nickelRemainder} remainder`)
+// console.log(`${pennies} pennies`)
+// console.log(`${pennyRemainder} remainder`)
+
