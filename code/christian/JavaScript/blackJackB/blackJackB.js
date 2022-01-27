206137f169e2fdeba3460355290f3b5eb874d39c
christian <chrisalv27@yahoo.com>
2022-01-27 14:44:36 -0800

    pick 3 lab

--- code/christian/JavaScript/blackJackB/blackJackB.js
+++ code/christian/JavaScript/blackJackB/blackJackB.js
@@ -1,59 +1,40 @@
-var cards = {
-    '2':2,
-    '3':3,
-    '4':4,
-    '5':5,
-    '6':6,
-    '7':7,
-    '8':8,
-    '9':9,
-    '10':10,
-}
 
-var face = {
-    'A':1,
-    'J':10,
-    'Q':10,
-    'K':10
-}
 
-let choice = prompt('What is your first card?: ')
-let choice2 = prompt('What is your second card?: ')
-let choice3 = prompt('What is your third card?: ')
+let addCardButton = document.querySelector('#add-card')
+let cardInput = document.querySelector('#drop-down')
+let adviseButton = document.querySelector('#get-advice')
+let advice = document.querySelector('#advice')
+let cardList = []
+addCardButton.addEventListener('click', function(){
+    let choice = cardInput.value
+    cardList.push(choice)
+    console.log(cardList)
 
-let total = 0 
+})
 
-if (choice in face){
-    firstCard = face[choice]
-}
-else{
-    firstCard = cards[choice]
-}
+adviseButton.addEventListener('click', function(){
+    function sum(numbers){
+        total_sum = 0
+        for (x of numbers){
+            total_sum += parseInt(x)
+        }
+        return total_sum
+    }
 
-if(choice2 in face){
-    secondCard = face[choice2]
-}
-else{
-    secondCard = cards[choice2]
-}
-if(choice3 in face){
-    thirdCard = face[choice3]
-}
-else{
-    thirdCard = cards[choice3]
-}
+    let result = sum(cardList)
+    console.log(result)
 
-result = (firstCard + secondCard + thirdCard)
 
 if (result < 17){
-    alert('Hit')
+    advice.innerText ='Hit'
 }
-else if (result >= 17 < 21){
-    alert('Stay')
+else if (result >= 17 && result < 21){
+    advice.innerText = 'Stay'
 }
-else if (result == 21){
-    alert('Blackjack!')
+else if (result === 21){
+    advice.innerText ='Blackjack!'
 }
-else{
-    alert('Already busted')
+else if (result >21){
+    advice.innerText ='Already busted'
 }
+})
