48f3ea21d58fe095fb360d85af17d1e37e868b22
Vanessa <v.d.chacon@gmail.com>
2022-01-27 09:32:13 -0800

    more updates

--- code/vanessa/Javascript/lab01/makechange/makechange.js
+++ code/vanessa/Javascript/lab01/makechange/makechange.js
@@ -3,12 +3,12 @@ let inputChange= parseFloat(prompt("What amount would you like to convert to cha
 inputChange= inputChange*100
 const quarters= Math.floor(inputChange/25)
 const quarterRemainder= inputChange % 25
-const dimes= Math.floor(inputChange/10)
-const dimeRemainder= inputChange % 10
-const nickels= Math.floor(inputChange/5)
-const nickelRemainder= inputChange % 5
-const pennies= Math.floor(inputChange/1)
-const pennyRemainder= inputChange % 1
+const dimes= Math.floor(quarterRemainder/10)
+const dimeRemainder= quarterRemainder % 10
+const nickels= Math.floor(dimeRemainder/5)
+const nickelRemainder= dimeRemainder % 5
+const pennies= Math.floor(nickelRemainder/1)
+const pennyRemainder= nickelRemainder % 1
 alert(inputChange)
 console.log("numberofquarters")
 console.log(quarters)
@@ -28,21 +28,4 @@ console.log("remainder")
 console.log(pennyRemainder)
 
 
-/*every_cent = dollar_amount * 100
-
-quarters = every_cent//25
-print(f"{quarters} quarters")
-quarters_remainder = every_cent % 25
-
-dimes = quarters_remainder//10
-print (f"{dimes} dimes")
-dimes_remainder = quarters_remainder % 10
-
-
-nickels = dimes_remainder//5
-print (f'{nickels} nickels')
-nickels_remainder = dimes_remainder%5
-
-pennies = nickels_remainder//1
-print (f'{pennies} pennies')
-pennies_remainder = nickels_remainder%5*/
+// for 1.98 (198) should have 7 quarters(1.75), 2 dimes (.20=1.95), 0 nickels, three pennies (.03=1.98).....need to fix after quarters/carryover remainder of quarters, etc.//
