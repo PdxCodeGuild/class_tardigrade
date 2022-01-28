3c7a900ab862c1f541290a8cdb89373219ee9c3d
Vanessa <v.d.chacon@gmail.com>
2022-01-27 16:32:19 -0800

    mob update

--- code/mobs/VCD-todo/todo.js
+++ code/mobs/VCD-todo/todo.js
@@ -1,15 +1,37 @@
-let toDoList=[]
+let toDoList = []
 let toDoButton = document.querySelector('#list')
 let item = document.querySelector('#item')
 let thingsList = document.querySelector('#things-list')
-toDoButton.addEventListener('submit', function(event) {
-			event.preventDefault()
-            let itemValue = item.value
-            toDoList.push(itemValue)
-            const listLi = document.createElement('li')
-            thingsList.appendChild(listLi)
-            console.log(toDoList)
-            listLi.innerText = itemValue
+toDoButton.addEventListener('submit', function (event) {
+    event.preventDefault()
+    let itemValue = item.value
+    toDoList.push(itemValue)
 
-})
-   
+    const listLi = document.createElement('li')
+    thingsList.appendChild(listLi)
+
+    const textSpan = document.createElement('span')
+    textSpan.innerText = itemValue
+
+    listLi.appendChild(textSpan)
+
+    const complete = document.createElement('button')
+    complete.innerText = "complete"
+    listLi.appendChild(complete)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = "delete"
+    listLi.appendChild(deleteButton)
+
+    console.log(toDoList)
+
+
+    complete.addEventListener('click', function () {
+        listLi.style.textDecoration = strike
+    })
+
+                
+            })
+
+            // deleteButton.addEventListener('click', function() {
+            //     listLi.style.textDecoration = strike
