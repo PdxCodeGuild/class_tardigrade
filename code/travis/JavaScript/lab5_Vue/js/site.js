92b7af415e813482f39ef1316094c1ffc1ae0e78
travisjackson3 <travisjackson3@gmail.com>
2022-01-28 16:28:44 -0800

    lab5 progress

---
+++ code/travis/JavaScript/lab5_Vue/js/site.js
@@ -0,0 +1,25 @@
+
+
+
+
+Vue.createApp({
+
+    data() {
+      return {
+        items: [{ todo: 'test1' }, { todo: 'test2' }]
+      }
+    },
+    methods: {
+
+    addToList() {
+
+        alert("test")
+
+        }
+
+    }
+
+
+    
+    
+  }).mount('#todo-list')
