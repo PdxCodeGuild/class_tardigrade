d1a140548fe2011e0c2c4a4605fcf88e156988c3
travisjackson3 <travisjackson3@gmail.com>
2022-01-31 21:25:05 -0800

    lab5 progress

--- code/travis/JavaScript/lab5_Vue/js/site.js
+++ code/travis/JavaScript/lab5_Vue/js/site.js
@@ -6,20 +6,49 @@ Vue.createApp({
 
     data() {
       return {
-        items: [{ todo: 'test1' }, { todo: 'test2' }]
+        
+        newTodo: "",
+        selectedRadio : "",
+        items: [{ id: 1, todo: 'test1' }, { id: 2, todo: 'test2' }]
       }
     },
     methods: {
 
-    addToList() {
+      addToList() {
+          
+          this.items.push({todo: this.newTodo})
+          this.newTodo = ""
+          },
 
-        alert("test")
-        this.items.push()
+      removeFromList(){
+                    //splice.("start index", "number of elements to remove beginning at start index")
+        let len = this.items.length
+        console.log(this.selectedRadio)
+        console.log(this.items.id)
+
+        for (let i =0; i < len; i++){
+
+          if (this.items.id == this.selectedRadio){
+
+
+          }
+
         }
+      //  this.items.splice((this.selectedRadio), 1)
+      //   console.log(this.selectedRadio)
+      //   console.log(this.todo)
+
+      },
+
+      completedItem(){
+        //line through completed items
+
+
+      }
+
 
     }
-
-
     
     
-  }).mount('#todo-list')
+  }).mount('#todo-list-app')
+
