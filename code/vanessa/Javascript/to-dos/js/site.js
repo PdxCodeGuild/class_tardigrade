ab6674414610ef02d8e3bc79be068d46299531a7
Vanessa <v.d.chacon@gmail.com>
2022-02-02 13:54:09 -0800

    testing

--- code/vanessa/Javascript/to-dos/js/site.js
+++ code/vanessa/Javascript/to-dos/js/site.js
@@ -3,7 +3,9 @@ const App = {
         return {
             message: 'Enter your to do list here:',
             itemsToDo: [],
-            newItem: ''
+            newItem: '',
+            itemsComplete: [],
+            completedItem: ''
         }
 
 
@@ -15,13 +17,20 @@ const App = {
             this.itemsToDo.push(this.newItem)
             this.newItem = ''
             
-        }
+        },
+        buttonComplete () {
+            this.buttonComplete.push(itemsCompleted),
+            this.completedItem = ''
+
+
+        },
+        buttonDelete () {
+           //need to fill in.
 
 
     }
 
-}
+},
 
 
 Vue.createApp(App).mount('#app')
-
