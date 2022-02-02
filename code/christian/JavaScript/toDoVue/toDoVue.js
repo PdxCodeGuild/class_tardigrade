216d21fb0b9f5666b9b09c6d856ed2c032e86ba4
christian <chrisalv27@yahoo.com>
2022-02-01 16:29:53 -0800

    vueToDo

--- code/christian/JavaScript/toDoVue/toDoVue.js
+++ code/christian/JavaScript/toDoVue/toDoVue.js
@@ -1,9 +1,25 @@
 const App = {
     data() {
         return {
-            message: 'Hello world!'
+            message: 'Hello world!',
+            taskList: [],
+            newTask: ''
         }
-    }
+    },
+
+    methods: {
+        addTask() {
+            this.taskList.push(this.newTask)
+            
+            this.newTask = ''
+        },
+        
+        completeTask: completeTask(event){
+
+        }
+
+
+    },
 }
 
 Vue.createApp(App).mount('#app')
