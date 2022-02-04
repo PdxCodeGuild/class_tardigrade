999a67ff12bec0f5384f245cb8894361ba70e8e4
Vanessa <v.d.chacon@gmail.com>
2022-02-04 10:19:57 -0800

    capstone setup

---
+++ code/vanessa/Javascript/MiniCap/mc.js
@@ -0,0 +1,30 @@
+const App = {
+    data() {
+        return {
+            message: 'hello',
+            randomQuote: '',
+            quote: []
+
+           
+        }
+
+    },
+    methods: {
+        getRandomJoke() { 
+			axios({
+				method: 'get',
+				url: 'https://favqs.com/api/qotd',
+				headers: { Accept: 'application/json' }
+			})
+			.then(response => { 
+				console.log(response.data) 
+				this.randomJoke = response.data.joke 
+				console.log(this)
+			})
+			
+		}
+    },
+}
+
+
+Vue.createApp(App).mount('#app')
