ac773ea3435e20010e6daf11ca013b37dedbc807
travisjackson3 <travisjackson3@gmail.com>
2022-02-03 16:28:50 -0800

    lab6

--- code/travis/JavaScript/lab6/js/site.js
+++ code/travis/JavaScript/lab6/js/site.js
@@ -14,10 +14,22 @@ Vue.createApp({
   data() {
     return {
       response: {},
-
       quotesData: [],
+      quoteList: [],
 
-      quoteList: []
+      searchText: "",
+
+      qotdQuote: {},
+      qotdAuthor: {},
+      keywordSearch: "",
+
+      searchList:[],
+      responseSearch: {},
+      quoteSearch:[],
+      searchData: [],
+
+      selected: "",
+
     }
   },
 
@@ -30,14 +42,28 @@ Vue.createApp({
       })
       .then(response => (this.response = response.data,
         this.quotesData = response.data.quotes
-      ))
+      )),
+
+
+
+      //Quote of the day
+  axios
+  .get('https://favqs.com/api/qotd', {
+    headers: {
+     
+    }
+  })
+  .then(response1 => (this.qotdQuote = response1.data.quote.body,
+    this.qotdAuthor = response1.data.quote.author
+  ))
+  console.log(this.qotd)
  
   },
 
   methods: {
 
     getQuotes() {
-
+      console.log("start load")
       let len = this.quotesData.length
 
       for (let i = 0; i < 1; i++){
@@ -47,10 +73,92 @@ Vue.createApp({
         this.quoteList.push({name: this.quotesData[i].author, quote: this.quotesData[i].body})
 
     }
+    },
+
+
+    searchQuotes(){
+
+
+
+      console.log(this.selected)
+      if(this.selected == "Keyword"){
+      searchKeyword = this.searchText
+        // Search for quotes containing the word "funny":
+        // GET https://favqs.com/api/quotes/?filter=funny
+
+        axios
+      .get(`https://favqs.com/api/quotes/?filter=${searchKeyword}`, {
+        headers: {
+          Authorization: "Token 15bcef41b1ec53efab73ce5f67267269"
+        }
+      })
+      .then(responseSearch => (this.responseSearch = responseSearch,
+        this.searchData = responseSearch.data.quotes
+      )).finally(() => {
+
+        //delays first
+       this.searchDisplay()
+      
+      })
     }
+    
+
+    //    console.log(this.searchData)
+
+
+    //  let len = this.searchData.length
+
+   //   for (let i = 0; i < len; i++){
+        
+
+        //adds quote and author name to list
+    //    this.searchList.push({name: this.searchData[i].author, quote: this.searchData[i].body})
+
+
+    //  }
+
+        // Get quotes that are tagged with "funny":        
+        // GET https://favqs.com/api/quotes/?filter=funny&type=tag
+
+        // Get quotes by Mark Twain:        
+        // GET https://favqs.com/api/quotes/?filter=Mark+Twain&type=author
+      },
+
+      //pagination
+    numPages(){
+  // quotesData[i].        
+
+      },
+
+      //waits for axios and then displays results
+      searchDisplay(){
+
+        this.searchList = []
+
+        let len = this.searchData.length
+  
+        for (let i = 0; i < len; i++){
+          
+  
+          //adds quote and author name to list
+          this.searchList.push({name: this.searchData[i].author, quote: this.searchData[i].body})
+  
+        }
+
+
+
+
+
+
+
+
+
+
 
     }
 
+
+    }
 
   
 
