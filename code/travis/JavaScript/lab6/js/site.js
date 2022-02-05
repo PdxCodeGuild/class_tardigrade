8bef5d39ef9a96de892f70ec0ded8ea9febe77c8
travisjackson3 <travisjackson3@gmail.com>
2022-02-04 16:32:38 -0800

    lab6work

--- code/travis/JavaScript/lab6/js/site.js
+++ code/travis/JavaScript/lab6/js/site.js
@@ -23,17 +23,27 @@ Vue.createApp({
       qotdAuthor: {},
       keywordSearch: "",
 
-      searchList:[],
+      searchList: [],
       responseSearch: {},
-      quoteSearch:[],
+      quoteSearch: [],
       searchData: [],
 
       selected: "",
 
+      lastPage: false,
+      quotesPage: "",
+
+      pageCountTest: 0
     }
   },
 
+
+
+  
   mounted() { // automatically runs on page load 
+
+
+    
     axios
       .get('https://favqs.com/api/quotes/', {
         headers: {
@@ -47,121 +57,150 @@ Vue.createApp({
 
 
       //Quote of the day
-  axios
-  .get('https://favqs.com/api/qotd', {
-    headers: {
-     
-    }
-  })
-  .then(response1 => (this.qotdQuote = response1.data.quote.body,
-    this.qotdAuthor = response1.data.quote.author
-  ))
-  console.log(this.qotd)
- 
+      axios
+        .get('https://favqs.com/api/qotd', {
+          headers: {
+
+          }
+        })
+        .then(response1 => (this.qotdQuote = response1.data.quote.body,
+          this.qotdAuthor = response1.data.quote.author
+        ))
+    console.log(this.qotd)
+
   },
 
   methods: {
+
 
     getQuotes() {
       console.log("start load")
       let len = this.quotesData.length
 
-      for (let i = 0; i < 1; i++){
-        
+      for (let i = 0; i < 1; i++) {
+
 
         //adds quote and author name to list
-        this.quoteList.push({name: this.quotesData[i].author, quote: this.quotesData[i].body})
+        this.quoteList.push({ name: this.quotesData[i].author, quote: this.quotesData[i].body })
 
-    }
+      }
+    },
+
+    //Searches API
+    searchQuotes() {
+
+
+      // Search for quotes containing the word "funny":
+      // GET https://favqs.com/api/quotes/?filter=funny
+      if (this.selected == "Keyword") {
+
+        searchEntry= this.searchText
+      }
+
+
+      // Get quotes by Mark Twain:        
+      // GET https://favqs.com/api/quotes/?filter=Mark+Twain&type=author
+
+      else if (this.selected == "Author") {
+        searchEntry = this.searchText + "&type=author"
+
+      }
+
+
+      // Get quotes that are tagged with "funny":        
+      // GET https://favqs.com/api/quotes/?filter=funny&type=tag
+
+      else if (this.selected == "Tag") {
+        searchEntry = this.searchText + "&type=tag"
+
+      }
+
+      axios
+        .get(`https://favqs.com/api/quotes/?filter=${searchEntry}`, {
+          headers: {
+            Authorization: "Token 15bcef41b1ec53efab73ce5f67267269"
+          }
+        })
+        .then(responseSearch => (this.responseSearch = responseSearch,
+          this.searchData = responseSearch.data.quotes,
+
+          //pagation test
+          this.lastPage = responseSearch.data.last_page,
+          this.quotesPage = responseSearch.data.page
+
+        )).finally(() => {
+
+          this.searchDisplay()
+
+          this.numPages()      
+
+
+
+          
+
+        })
     },
 
 
-    searchQuotes(){
+  //pagination
+  numPages() {
 
 
 
-      console.log(this.selected)
-      if(this.selected == "Keyword"){
-      searchKeyword = this.searchText
-        // Search for quotes containing the word "funny":
-        // GET https://favqs.com/api/quotes/?filter=funny
+    if(this.lastPage === true){
+      console.log("test last page" + this.lastPage)
 
-        axios
-      .get(`https://favqs.com/api/quotes/?filter=${searchKeyword}`, {
-        headers: {
-          Authorization: "Token 15bcef41b1ec53efab73ce5f67267269"
-        }
-      })
-      .then(responseSearch => (this.responseSearch = responseSearch,
-        this.searchData = responseSearch.data.quotes
-      )).finally(() => {
 
-        //delays first
-       this.searchDisplay()
+
+
+
+    // quotesData[i]. 
+    // pageCount = 1
+    // while(pageCount < 5){
+
+
+    // axios
+    // .get(`https://favqs.com/api/quotes/?page=${pageCount}`, {
+    //   headers: {
+    //     Authorization: "Token 15bcef41b1ec53efab73ce5f67267269"
+    //   }
+    // })
+    // .then(responseLastPage => (this.lastPage = responseLastPage.data.last_page,
+    //   this.quotesPage = responseLastPage.data.page
+    // )).finally(() =>{
       
-      })
-    }
-    
-
-    //    console.log(this.searchData)
-
-
-    //  let len = this.searchData.length
-
-   //   for (let i = 0; i < len; i++){
-        
-
-        //adds quote and author name to list
-    //    this.searchList.push({name: this.searchData[i].author, quote: this.searchData[i].body})
-
-
-    //  }
-
-        // Get quotes that are tagged with "funny":        
-        // GET https://favqs.com/api/quotes/?filter=funny&type=tag
-
-        // Get quotes by Mark Twain:        
-        // GET https://favqs.com/api/quotes/?filter=Mark+Twain&type=author
-      },
-
-      //pagination
-    numPages(){
-  // quotesData[i].        
-
-      },
-
-      //waits for axios and then displays results
-      searchDisplay(){
-
-        this.searchList = []
-
-        let len = this.searchData.length
-  
-        for (let i = 0; i < len; i++){
-          
-  
-          //adds quote and author name to list
-          this.searchList.push({name: this.searchData[i].author, quote: this.searchData[i].body})
-  
-        }
+    //   console.log(" finally page num:" + this.quotesPage)
+    //   console.log("finally last page:" + this.lastPage)
+    //   pageCount += 1
+    // })
 
 
 
+    //console.log("page num:" + this.quotesPage)
+  //  console.log("last page:" + this.lastPage)
+ 
+}
+  },
+
+  //waits for axios response and then displays results
+  searchDisplay() {
+
+    this.searchList = []
+
+    let len = this.searchData.length
+
+    for (let i = 0; i < len; i++) {
 
 
-
-
-
-
-
+      //adds quote and author name to list
+      this.searchList.push({ name: this.searchData[i].author, quote: this.searchData[i].body })
 
     }
 
 
-    }
+  }
 
-  
-
+  }
 
 
 }).mount('#app')
