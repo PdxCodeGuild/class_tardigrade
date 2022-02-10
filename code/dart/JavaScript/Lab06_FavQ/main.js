1b2b121b693d1225e1809cce74bf19437edbae42
d'Artagnan Childers <dartagnanchilders@dArtagnans-MacBook-Pro.local>
2022-02-09 16:25:22 -0800

    Capstone

--- code/dart/JavaScript/Lab06_FavQ/main.js
+++ code/dart/JavaScript/Lab06_FavQ/main.js
@@ -23,7 +23,7 @@ const App = {
                 method: 'get',
                 url: 'https://favqs.com/api/qotd',
             }).then(response => { 
-                this.randomQuote = response.data.quotes 
+                this.randomQuote = response.data.quote
             })
         },
         searchKeyword() {
@@ -31,12 +31,10 @@ const App = {
                 method: 'get',
                 url: 'https://favqs.com/api/quotes/?',
                 headers: { 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"' },
-                params: { filter: this.Keyword },
+                params: { filter: this.Keyword, type: 'keyword' },
             }).then(response => {
-                console.log(response.data)
                 this.foundQuotes = response.data.quotes
                 this.pageNumber = response.data.page
-                console.log(this.pageNumber)
             })
         },
         searchTag() {
@@ -44,9 +42,8 @@ const App = {
                 method: 'get',
                 url: 'https://favqs.com/api/quotes/?',
                 headers: { 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"' },
-                params: { filter: this.Tag },
+                params: { filter: this.Tag, type: 'tag' },
             }).then(response => {
-                console.log(response.data)
                 this.foundQuotes = response.data.quotes
                 this.pageNumber = response.data.page
             })
@@ -56,14 +53,73 @@ const App = {
                 method: 'get',
                 url: 'https://favqs.com/api/quotes/?',
                 headers: { 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"' },
-                params: { filter: this.Author },
+                params: { filter: this.Author, type: 'author'},
             }).then(response => {
-                console.log(response.data)
                 this.foundQuotes = response.data.quotes
                 this.pageNumber = response.data.page
             })
         },
-    }
+        nextPage: function() {
+            this.pageNumber += 1
+            if (this.userKeyword != '') {
+                this.nextPageUrl = 'https://favqs.com/api/quotes/?'
+                this.myFilter = this.userKeyword
+            } 
+            else if (this.userTag != '') {
+                this.nextPageUrl = 'https://favqs.com/api/quotes/?filter=filter&type=tag'
+                this.myFilter = this.userTag
+            }
+            else if (this.userAuthor != '') {
+                this.nextPageUrl = 'https://favqs.com/api/quotes/?filter=filter&type=author'
+                this.myFilter = this.userAuthor
+            }
+            axios({
+                method: 'get',
+                url: this.nextPageUrl,
+                headers: {
+                    'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"',
+                },
+                params: {
+                    filter: this.myFilter,
+                    page: this.pageNumber
+                },
+            }).then(response => {
+                this.foundQuotes = response.data.quotes
+                this.pageNumber = response.data.page
+            })
+        },
+        previousPage: function() {
+            this.pageNumber -= 1
+            if (this.userKeyword != '') {
+                this.nextPageUrl = 'https://favqs.com/api/quotes/?'
+                this.myFilter = this.userKeyword
+            } 
+            else if (this.userTag != '') {
+                this.nextPageUrl = 'https://favqs.com/api/quotes/?filter=filter&type=tag'
+                this.myFilter = this.userTag
+            }
+            else if (this.userAuthor != '') {
+                this.nextPageUrl = 'https://favqs.com/api/quotes/?filter=filter&type=author'
+                this.myFilter = this.userAuthor
+            }
+            axios({
+                method: 'get',
+                url: this.nextPageUrl,
+                headers: {
+                    'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"',
+                },
+                params: {
+                    filter: this.myFilter,
+                    page: this.pageNumber
+                },
+            }).then(response => {
+                this.foundQuotes = response.data.quotes
+                this.pageNumber = response.data.page
+            })
+
+        }
+
+    },
 }
 
 Vue.createApp(App).mount('#app')
