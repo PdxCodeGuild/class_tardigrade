b7189e93532de4b8667dddc8868ebd3b0334d02c
Shad-Wi <94399458+Shad-Wi@users.noreply.github.com>
2022-02-13 08:51:47 -0500

    oo

--- code/Shad/JAVASCRIPT/VUETODO.js
+++ code/Shad/JAVASCRIPT/VUETODO.js
@@ -7,9 +7,55 @@ const App = {
 
         return {
 
-            lis: [ 'midori', 'aoi','shiro','kuro'],
+
+            lis: [
+                {
+                    item: 'midori',
+                    status: false
+
+                },
+                // {
+                //     item: 'aoi',
+                //     status: true
+                // },
+
+
+                // {
+                //     item: 'shiro',
+                //     status: false
+                // },
+
+                // {
+                //     item: 'kuro',
+                //     status: false
+                // }
+
+
+
+
+
+            ],
+            // done :[
+            //     {
+            //         item:'' ,
+            //         status: false
+
+            //     }
+            // ],
+            
 
             write: '',
+
+
+            still() {
+                return this.lis.filter(lis => lis.completed === false)
+            },
+
+            done() {
+                return this.lis.filter(lis => lis.completed === true)
+
+
+            }
 
 
         }
@@ -24,8 +70,12 @@ const App = {
 
         add() {
 
-            this.lis.push(this.write)
-            this.write = ''
+            this.lis.push({
+                item: this.write,
+                status: false
+            })
+
+            // this.write = ''
         },
 
 
@@ -33,21 +83,36 @@ const App = {
 
             // this.lis.pop(write)
             // this.delete lis[this.write]
-            
+            let index = this.lis.indexOf(lis)
 
             this.lis.splice(this.index, 1);
+            
 
         },
 
-        complete() {
-            console.log(lis)
+        complete(lis) {
+            // lis.style.color = 'aqua',
+            lis.status = !lis.status
+            // this.done.push(this.index, 1)
+            // this.done.push({
+            //     item: this.lis,
+            //     status: true
+            // })
+
+
+
+
+            // toogle() {
+            //     lis.status = !lis.status
+            // }
+            
+
         }
 
+
+
+
     }
-
-
-
 }
 
-
-Vue.createApp(App).mount('#app')
+const app = Vue.createApp(App).mount('#app')
