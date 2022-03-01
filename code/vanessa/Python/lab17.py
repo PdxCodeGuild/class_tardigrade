9dfe5b7b6597e8981c0771f045c85b260870d994
Vanessa <v.d.chacon@gmail.com>
2022-03-01 12:28:09 -0800

    lab 17 complete

--- code/vanessa/Python/lab17.py
+++ code/vanessa/Python/lab17.py
@@ -1,3 +1,4 @@
+from email.quoprimime import quote
 import requests
 import time
 
@@ -15,3 +16,25 @@ response = requests.get(url,headers=headers,)
 data = response.json()
 print(data["quote"]['body'])
 print(data["quote"]["author"])
+
+#-------------------------------------------------------------------------
+
+def get_quotes(quote_type):
+    headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
+    params = {'quote_type': quote_type}
+    page = 1
+    response = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={quote_type}', headers=headers, params=params)
+
+    data = response.json()
+   
+
+    for i, quote in enumerate(data.get('quotes', [])): # person.get('name', 'John Doe') returns the name if it exists as a key, if not returns 'John Doe'
+        print(f'\n{i + 1}:')
+        print(quote.get('body','author'))
+
+
+while True:
+    quote_type = input('Enter a keyword to search for quotes or type "done" to exit): ')
+    if quote_type == 'done':
+        break
+    get_quotes(quote_type)
