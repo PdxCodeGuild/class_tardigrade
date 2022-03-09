2c08c0fc2dff1babb0a8e8b33dc6c01f2afb52a3
christian <chrisalv27@yahoo.com>
2022-03-09 12:38:36 -0800

    vue app

---
+++ code/christian/JavaScript/quotesVue/quotesVue.js
@@ -0,0 +1,39 @@
+const App = {
+    data (){
+        return {
+            message: 'Quotes',
+            searchWord: '',
+            randomQuote: '',
+            quotes: []
+
+           }
+    },
+    methods: {
+
+        getRandomQuote() {
+            axios({
+                method: 'get',
+                urls: 'https://favqs.com/api/quotes',
+                headers: {Accept: 'application/json'}
+            })
+            .then(response => {
+                this.randomQuote = response.data.quotes
+
+            })
+        },
+        getQuote(){
+            axios({
+                method: 'get',
+                url: 'https://favqs.com/api/quotes',
+                headers: {Accept: 'application/json'},
+                params: { searchWord: this.searchWord} 
+            })
+            .then(response => {
+                console.log(response.data)
+                this.quotes = response.data.results
+            })
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
