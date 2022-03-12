688c440d319bb179ddfd532bcba4997f60d48ffb
christian <chrisalv27@yahoo.com>
2022-03-11 17:20:29 -0800

    contactlist

--- code/christian/python/contact_list.py
+++ code/christian/python/contact_list.py
@@ -1,11 +1,11 @@
-
+filepath = 'code/christian/python/superheroes.csv'
 
 with open('superheroes.csv', 'r') as f:
     heroes = f.read().split('\n')
 # print(heroes, 'heroes')
 
 headers = heroes[0].split(',')
-# print(headers,'headers')
+# print(headers,'headers!')
 marvel = heroes[1:]
 # print(marvel,'marvel')
 
@@ -13,7 +13,7 @@ hero_list = []
 for hero in marvel:
     hero = hero.split(',')
     hero_dict = {}
-    hero_dict= dict(zip(headers, marvel))
+    hero_dict= dict(zip(headers, hero))
     hero_list.append(hero_dict)
 
 
@@ -26,17 +26,22 @@ def create_hero():
         hero_dict[header] = prompt
     hero_list.append(hero_dict)
 
-def retrieve(name):
+def fetch(name):
+    # print(name, 'this should be my input')
     global hero_list
+    # print(hero_list, 'hero list')
     for hero in hero_list:
         if hero['name'] == name:
             return hero
 
+
 def update(hero):
     global headers
     for header in headers:
-        update = input(f'Enter an update for {header} (currently {hero[header]}')
+        print(hero, '!!!!!!!!!!!!!!!!!!')
+        update = input(f'Enter an update for {header} (currently {hero[header]}): ')
         hero[header] = update
+        
 
 def delete(hero):
     global hero_list
@@ -45,7 +50,7 @@ def delete(hero):
 
 
 while True:
-    user = input('Do you want to create, retrieve, update, or delete a hero? or enter "done" to quit')
+    user = input('Do you want to create, retrieve, update, or delete a hero? or enter "done" to quit: ')
     if user == 'done':
         print('Goodbye!')
         break
@@ -53,20 +58,32 @@ while True:
         create_hero()
     elif user == 'retrieve':
         hero_name = input('Which hero do you want to retrieve?: ')
-        hero = retrieve(hero) 
+        hero = fetch(hero_name) 
+        if hero is None:
+            print('Does not exist.')
+            continue
         for header in headers:
-            print(f'{header}... {hero[header]}')
+            print(f'{header}: {hero[header]}')
     elif user == 'update':
         hero_name = input('Which hero do you want to update?: ')
-        hero = retrieve(hero)
+        hero = fetch(hero_name)
+        
         update(hero)
     elif user == 'delete':
-        user = input('Which hero do you want to delete?: ')
-        hero = retrieve(hero)
+        hero_name = input('Which hero do you want to delete?: ')
+        hero = fetch(hero_name)
         delete(hero)
 
 
+csv = ','.join(headers)
+for hero in hero_list: 
+    csv += '\n' 
+    csv += ','.join(hero.values()) 
 
+# print(csv)
+
+with open('superheroes.csv', 'w') as f:
+    f.write(csv)
 
 
 
@@ -83,6 +100,9 @@ while True:
 # Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
 # Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list
     
-        
+# name,power,enemy,alias
+# spiderman,senses,venom,peter
+# superman,everything,lexluthor,clark
+# batman,money,joker,bruce        
   
 
