fef9186fa70f97261a9ccafd5a206d0d3f6023c2
christian <chrisalv27@yahoo.com>
2022-03-28 13:34:12 -0700

    labs

--- code/christian/django/chirp/posts/models.py
+++ code/christian/django/chirp/posts/models.py
@@ -7,16 +7,15 @@ from django.contrib.auth.models import User
 
 #Tweet Post model
 
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)     
     body = models.CharField(max_length= 500)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created = models.DateTimeField(default=timezone.now)
     def __str__(self):
-        return f'{self.title},{self.user},{self.body}'
+        return f'{self.title},{self.author},{self.body}'
     
-    def get_absolute_url(self):
-        return reverse("posts_detail", kwargs={"pk": self.pk}) #return detail post 
+     
     
     
     
--- code/christian/django/chirp/posts/urls.py
+++ code/christian/django/chirp/posts/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'posts'
+app_name = 'post'
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('post/<int:pk>/', views.PostDetail.as_view(), name = 'detail'),
-    path('post/new/', views.PostCreate.as_view(), name='new'),
-    path('post/<int:pk>/edit/', views.PostEdit.as_view(), name='edit'),
-    path('post/<int:pk>/delete/', views.PostDelete.as_view(), name = 'delete')
+    path('', views.HomeView.as_view(), name='home'),
+    # path('post/<int:pk>/', views.PostDetail.as_view(), name = 'detail'),
+    # path('post/new/', views.PostCreate.as_view(), name='new'),
+    # path('post/<int:pk>/edit/', views.PostEdit.as_view(), name='edit'),
+    # path('post/<int:pk>/delete/', views.PostDelete.as_view(), name = 'delete')
 ]
